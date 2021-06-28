aec9df567495335b54c1d0137c92bbf94f40df21
rhenck <richard.henck@iqb.hu-berlin.de>
2021-06-28 11:45:25 +0200

    Refactor style initialisation

--- projects/common/canvas-element-component.directive.ts
+++ projects/common/canvas-element-component.directive.ts
@@ -1,16 +1,20 @@
 import {
-  Directive, Output, EventEmitter, Input
+  Directive, Output, EventEmitter, Input, OnInit
 } from '@angular/core';
 import { UnitUIElement } from './unit';
 
 @Directive()
-export abstract class CanvasElementComponent {
-  @Input() elementModel!: UnitUIElement;
+export abstract class CanvasElementComponent implements OnInit {
+  @Input() elementModel: UnitUIElement = {} as UnitUIElement;
   @Input() draggable: boolean = false;
   @Output() elementSelected = new EventEmitter<{ componentElement: CanvasElementComponent, multiSelect: boolean }>();
 
-  style: Record<string, string> = {};
   _selected = false;
+  style: Record<string, string> = {};
+
+  ngOnInit(): void {
+    this.updateStyle();
+  }
 
   set selected(newValue: boolean) {
     this._selected = newValue;

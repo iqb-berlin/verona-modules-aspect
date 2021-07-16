84dae27c1550388704dc38d1d168ef046d6b4bcd
jojohoch <joachim.hoch@iqb.hu-berlin.de>
2021-07-16 12:07:36 +0200

    Fix label of `RadioButtonGroupComponent`
    Use `elementModel.label` instead of `elementModel.text`

--- projects/common/element-components/radio-button-group.component.ts
+++ projects/common/element-components/radio-button-group.component.ts
@@ -14,7 +14,7 @@ import { FormElementComponent } from '../form-element-component.directive';
            [style.font-weight]="elementModel.bold ? 'bold' : ''"
            [style.font-style]="elementModel.italic ? 'italic' : ''"
            [style.text-decoration]="elementModel.underline ? 'underline' : ''">
-          <label id="radio-group-label">{{elementModel.text}}</label>
+          <label id="radio-group-label">{{elementModel.label}}</label>
           <mat-radio-group aria-labelledby="radio-group-label" fxLayout="{{elementModel.alignment}}"
                            [formControl]="formControl">
               <mat-radio-button *ngFor="let option of elementModel.options" [value]="option">

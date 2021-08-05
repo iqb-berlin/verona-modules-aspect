dc27dfd162ebf1c7dc8c8fb8dad0644d878ad5a0
jojohoch <joachim.hoch@iqb.hu-berlin.de>
2021-08-05 09:30:53 +0200

    [player] Rename properties in `PlayerStateComponent`

--- projects/player/src/app/components/player-state/player-state.component.ts
+++ projects/player/src/app/components/player-state/player-state.component.ts
@@ -22,14 +22,14 @@ export class PlayerStateComponent implements OnInit, OnDestroy {
   @Input() parenForm!: FormGroup;
   @Input() pages!: UnitPage[];
 
-  currentIndex: number = 0;
-  pageIndices!: number[];
+  currentPlayerPageIndex: number = 0;
+  playerPageIndices!: number[];
   scrollPages!: UnitPage[];
   hasScrollPages!: boolean;
   pageWidth!: number;
   validPages!: Record<string, string> [];
   alwaysVisiblePage!: UnitPage | undefined;
-  alwaysVisiblePageIndex!: number;
+  alwaysVisibleUnitPageIndex!: number;
   running: boolean = true;
 
   private ngUnsubscribe = new Subject<void>();
@@ -61,19 +61,19 @@ export class PlayerStateComponent implements OnInit, OnDestroy {
   }
 
   private initPageState() {
-    this.alwaysVisiblePageIndex = this.pages.findIndex((page: UnitPage): boolean => page.alwaysVisible);
-    this.alwaysVisiblePage = this.pages[this.alwaysVisiblePageIndex];
+    this.alwaysVisibleUnitPageIndex = this.pages.findIndex((page: UnitPage): boolean => page.alwaysVisible);
+    this.alwaysVisiblePage = this.pages[this.alwaysVisibleUnitPageIndex];
     this.scrollPages = this.pages.filter((page: UnitPage): boolean => !page.alwaysVisible);
     this.hasScrollPages = this.scrollPages && this.scrollPages.length > 0;
     this.pageWidth = !this.alwaysVisiblePage || !this.hasScrollPages ? 100 : 50;
     this.validPages = this.scrollPages.map((page: UnitPage, index: number): Record<string, string> => (
       { [page.id]: `${this.translateService.instant('pageIndication', { index: index + 1 })}` }));
-    this.pageIndices = this.pages.map(
+    this.playerPageIndices = this.pages.map(
       (page: UnitPage, index: number): number => {
-        if (index === this.alwaysVisiblePageIndex) {
+        if (index === this.alwaysVisibleUnitPageIndex) {
           return this.pages.length - 1;
         }
-        return (this.alwaysVisiblePageIndex < 0 || index < this.alwaysVisiblePageIndex) ? index : index - 1;
+        return (this.alwaysVisibleUnitPageIndex < 0 || index < this.alwaysVisibleUnitPageIndex) ? index : index - 1;
       }
     );
   }
@@ -112,13 +112,13 @@ export class PlayerStateComponent implements OnInit, OnDestroy {
   private onPageNavigation(message: VopPageNavigationCommand): void {
     // eslint-disable-next-line no-console
     console.log('player: onPageNavigation', message);
-    this.currentIndex = parseInt(message.target, 10);
+    this.currentPlayerPageIndex = parseInt(message.target, 10);
   }
 
   private sendVopStateChangedNotification(): void {
     const playerState: PlayerState = {
       state: this.state,
-      currentPage: this.currentIndex.toString(10),
+      currentPage: this.currentPlayerPageIndex.toString(10),
       validPages: this.validPages
     };
     this.veronaPostService.sendVopStateChangedNotification({ playerState });

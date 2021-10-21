6f1d8ad952fcac8f70025b5d1b29f7c2c8caad1b
jojohoch <joachim.hoch@iqb.hu-berlin.de>
2021-10-21 09:44:50 +0200

    [player] Add media element interface

--- projects/common/interfaces/UIElementInterfaces.ts
+++ projects/common/interfaces/UIElementInterfaces.ts
@@ -10,3 +10,24 @@ export interface FontElement {
 export interface SurfaceUIElement {
   backgroundColor: string;
 }
+
+export interface MediaElement {
+  autostart: boolean; // default: false
+  autostartDelay: number; // default: 0 (milliseconds)
+  loop: boolean; // false
+  startControl: boolean; // true
+  pauseControl: boolean; // false
+  stopControl: boolean; // false
+  progressBar: boolean; // true
+  interactiveProgressbar: boolean; // false
+  volumeControl: boolean; // true
+  hintLabel: string; // ''
+  hintLabelDelay: number; // default: 0 (milliseconds)
+  uninterruptible: boolean; // false kein Blättern; starten eines anderen Videos; ....
+  hideOtherPages: boolean; // false (Solange nicht vollständig gespielt, sind alle anderen Seiten verborgen)
+  activeAfter: string; // '' (andere Audio - id; Audio ist deaktiviert, solange anderes nicht vollständig abgespielt)
+  minRuns: number; // 1
+  maxRuns: number | null; // null
+  showRestRuns: boolean; // true (wenn maxRuns)
+  showRestTime: boolean; // true
+}

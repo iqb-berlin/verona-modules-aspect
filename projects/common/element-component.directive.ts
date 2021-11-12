de3be1d877fd586c8f1aafe1ef3383677199b0e5
jojohoch <joachim.hoch@iqb.hu-berlin.de>
2021-11-12 11:05:51 +0100

    Detect whether elements are displayed in the player or in the editor

--- projects/common/element-component.directive.ts
+++ projects/common/element-component.directive.ts
@@ -1,15 +1,21 @@
 import {
+  AfterViewInit,
   Directive, ElementRef
 } from '@angular/core';
 import { UIElement } from './models/uI-element';
 
 @Directive()
-export abstract class ElementComponent {
+export abstract class ElementComponent implements AfterViewInit {
   abstract elementModel: UIElement;
+  project!: 'player' | 'editor';
 
   constructor(private elementRef: ElementRef) {}
 
   get domElement(): Element {
     return this.elementRef.nativeElement;
+  }
+
+  ngAfterViewInit(): void {
+    this.project = this.elementRef.nativeElement.closest('app-element-container') ? 'player' : 'editor';
   }
 }

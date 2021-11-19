13f35d1d7ddde14f12482fc6449c6e02e2b922ae
mechtelm <nicht@mehr.fragen>
2021-11-19 09:23:30 +0100

    Implement 'readonly' and 'required' attributes to slider

--- projects/common/element-components/slider.component.ts
+++ projects/common/element-components/slider.component.ts
@@ -1,24 +1,49 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import {Component, ElementRef, EventEmitter, OnInit, Output, ViewChild} from '@angular/core';
 import { ElementComponent } from '../element-component.directive';
 import { SliderElement } from '../models/slider-element';
 import { FormElementComponent } from '../form-element-component.directive';
+import {ValidatorFn, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-slider',
   template: `
     <div [style.display]="'flex'"
+         [style.flex-direction]="'row'"
          [style.background-color]="elementModel.backgroundColor"
          [style.width.%]="100"
          [style.height.%]="100">
-      <div *ngIf="elementModel.showValues">{{elementModel.minValue | number:'.0'}}</div>
-      <mat-slider
-              [formControl]="elementFormControl"
-              [style.width.%]="100"
-              [style.height.%]="100"
-              [max]="elementModel.maxValue"
-              [min]="elementModel.minValue">
-      </mat-slider>
-      <div *ngIf="elementModel.showValues">{{elementModel.maxValue | number:'.0'}}</div>
+      <div *ngIf="elementModel.showValues"
+           [style.color]="elementModel.fontColor"
+           [style.font-family]="elementModel.font"
+           [style.font-size.px]="elementModel.fontSize"
+           [style.line-height.%]="elementModel.lineHeight"
+           [style.font-weight]="elementModel.bold ? 'bold' : ''"
+           [style.font-style]="elementModel.italic ? 'italic' : ''"
+           [style.text-decoration]="elementModel.underline ? 'underline' : ''">{{elementModel.minValue | number:'.0'}}</div>
+      <div [style.display]="'flex'"
+           [style.flex-direction]="'column'"
+           [style.width.%]="100"
+           [style.height.%]="100">
+        <mat-slider
+          [formControl]="elementFormControl"
+          [disabled]="elementModel.readOnly"
+          [style.width.%]="100"
+          [max]="elementModel.maxValue"
+          [min]="elementModel.minValue">
+        </mat-slider>
+        <mat-error *ngIf="elementFormControl.touched && elementFormControl.errors">
+          {{elementFormControl.errors | errorTransform: elementModel}}
+        </mat-error>
+      </div>
+      <div *ngIf="elementModel.showValues"
+           [style.color]="elementModel.fontColor"
+           [style.font-family]="elementModel.font"
+           [style.font-size.px]="elementModel.fontSize"
+           [style.line-height.%]="elementModel.lineHeight"
+           [style.font-weight]="elementModel.bold ? 'bold' : ''"
+           [style.font-style]="elementModel.italic ? 'italic' : ''"
+           [style.text-decoration]="elementModel.underline ? 'underline' : ''"
+      >{{elementModel.maxValue | number:'.0'}}</div>
     </div>
   `,
   styles: [
@@ -29,4 +54,14 @@ import { FormElementComponent } from '../form-element-component.directive';
 })
 export class SliderComponent extends FormElementComponent {
   elementModel!: SliderElement;
+  // todo: ?? setting disabled attribute of slider may cause 'changed after checked' error
+  // todo: localize 'number' pipe to show '.' instead of ',' by values greater then 999
+
+  get validators(): ValidatorFn[] {
+    const validators: ValidatorFn[] = [];
+    if (this.elementModel.required) {
+      validators.push(Validators.min(this.elementModel.minValue + 1));
+    }
+    return validators;
+  }
 }
--- projects/common/models/slider-element.ts
+++ projects/common/models/slider-element.ts
@@ -1,17 +1,31 @@
-import { SurfaceUIElement } from '../interfaces/UIElementInterfaces';
+import {FontElement, SurfaceUIElement} from '../interfaces/UIElementInterfaces';
 import { InputElement, UIElement} from './uI-element';
-import { initSurfaceElement } from '../util/unit-interface-initializer';
+import {initFontElement, initSurfaceElement} from '../util/unit-interface-initializer';
 
-export class SliderElement extends InputElement implements SurfaceUIElement {
+export class SliderElement extends InputElement implements FontElement, SurfaceUIElement {
   minValue: number = 0;
   maxValue: number = 100;
   showValues: boolean = true;
 
-  backgroundColor: string = 'transparent';
+  bold: boolean = false;
+  font: string = 'Roboto';
+  fontColor: string = 'black';
+  fontSize: number = 18;
+  italic: boolean = false;
+  lineHeight: number = 120;
+  underline: boolean = false;
+
+  backgroundColor: string = '#AAA0';
 
   constructor(serializedElement: UIElement) {
     super(serializedElement);
     Object.assign(this, serializedElement);
+    Object.assign(this, initFontElement(serializedElement));
+    if (!serializedElement.backgroundColor) {
+      serializedElement.backgroundColor = '#d3d3d300';
+    }
     Object.assign(this, initSurfaceElement(serializedElement));
+    // todo: delete this.label --> label must be declared as optional
   }
+
 }

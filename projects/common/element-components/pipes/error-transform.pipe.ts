75b71f2525d1d9a7dd6aa4d2c1e77f6b16c172e9
jojohoch <joachim.hoch@iqb.hu-berlin.de>
2021-11-19 15:46:45 +0100

    Refactor error transform pipe
    - Remove fallback of TranslationService to allow empty error messages
      that can still cause red borders
    - Remove the case distinction for checkboxes, since the requiredTrue
      validator also sends required as errorKey and a special fallback text
      is no longer used

--- projects/common/element-components/pipes/error-transform.pipe.ts
+++ projects/common/element-components/pipes/error-transform.pipe.ts
@@ -1,14 +1,11 @@
 import { Pipe, PipeTransform } from '@angular/core';
 import { ValidationErrors } from '@angular/forms';
-import { TranslateService } from '@ngx-translate/core';
 import { UIElement } from '../../models/uI-element';
 
 @Pipe({
   name: 'errorTransform'
 })
 export class ErrorTransformPipe implements PipeTransform {
-  constructor(private translateService: TranslateService) {}
-
   transform(validationErrors: ValidationErrors, elementModel: UIElement): string {
     const validationMessages = this.getValidationMessages(elementModel);
     let returnMessage = '';
@@ -17,24 +14,15 @@ export class ErrorTransformPipe implements PipeTransform {
       if (returnMessage) {
         returnMessage += '; ';
       }
-      const messageKey = errorKey === 'required' && elementModel.type === 'checkbox' ? 'requiredTrue' : errorKey;
-      returnMessage += validationMessages[messageKey];
+      returnMessage += validationMessages[errorKey];
     });
     return returnMessage;
   }
 
-  private getValidationMessages(elementModel: UIElement): Record<string, string> {
-    return {
-      required: elementModel.requiredWarnMessage ||
-        this.translateService.instant('validators.inputRequired'),
-      requiredTrue: elementModel.requiredWarnMessage ||
-        this.translateService.instant('validators.inputRequiredTrue'),
-      minlength: elementModel.minWarnMessage ||
-        this.translateService.instant('validators.inputTooShort'),
-      maxlength: elementModel.maxWarnMessage ||
-        this.translateService.instant('validators.inputTooLong'),
-      pattern: elementModel.patternWarnMessage ||
-        this.translateService.instant('validators.wrongPattern')
-    };
-  }
+  private getValidationMessages = (elementModel: UIElement): Record<string, string> => ({
+    required: elementModel.requiredWarnMessage as string,
+    minlength: elementModel.minWarnMessage as string,
+    maxlength: elementModel.maxWarnMessage as string,
+    pattern: elementModel.patternWarnMessage as string
+  });
 }

91b797bfb5e254101a12df261813715147578929
rhenck <richard.henck@iqb.hu-berlin.de>
2021-11-29 12:28:24 +0100

    Add styling props to simple text field

--- projects/common/ui-elements/textfield-simple/text-field-simple-element.ts
+++ projects/common/ui-elements/textfield-simple/text-field-simple-element.ts
@@ -1,9 +1,22 @@
-import { InputElement, UIElement } from '../../models/uI-element';
+import {
+  FontElement,
+  FontProperties,
+  InputElement,
+  SurfaceElement,
+  SurfaceProperties,
+  UIElement
+} from '../../models/uI-element';
+import { initFontElement, initSurfaceElement } from '../../util/unit-interface-initializer';
 
-export class TextFieldSimpleElement extends InputElement {
+export class TextFieldSimpleElement extends InputElement implements FontElement, SurfaceElement{
+  fontProps: FontProperties;
+  surfaceProps: SurfaceProperties;
+
   constructor(serializedElement: UIElement) {
     super(serializedElement);
     Object.assign(this, serializedElement);
+    this.fontProps = initFontElement(serializedElement);
+    this.surfaceProps = initSurfaceElement(serializedElement);
 
     this.height = serializedElement.height || 25;
   }

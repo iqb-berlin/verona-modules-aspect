d104d048d02ac93195b56a0c5a7d9373d8bbc8c4
jojohoch <joachim.hoch@iqb.hu-berlin.de>
2021-11-30 09:36:36 +0100

    Add PositionProperties to ImageElement

--- projects/common/ui-elements/image/image-element.ts
+++ projects/common/ui-elements/image/image-element.ts
@@ -1,14 +1,19 @@
-import { UIElement } from '../../models/uI-element';
+import { PositionedElement, PositionProperties, UIElement } from '../../models/uI-element';
+import { initPositionedElement } from '../../util/unit-interface-initializer';
 
-export class ImageElement extends UIElement {
+export class ImageElement extends UIElement implements PositionedElement {
   src: string = '';
   magnifier: boolean = false;
   magnifierSize: number = 100;
   magnifierZoom: number = 1.5;
   magnifierUsed: boolean = false;
+
+  positionProps: PositionProperties;
+
   constructor(serializedElement: UIElement) {
     super(serializedElement);
     Object.assign(this, serializedElement);
+    this.positionProps = initPositionedElement(serializedElement);
     this.height = serializedElement.height || 100;
   }
 }

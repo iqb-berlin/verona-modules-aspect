129161024587fba0ca08e8df314a97b34e8027e9
jojohoch <joachim.hoch@iqb.hu-berlin.de>
2021-12-10 10:22:15 +0100

    [player] Fix position of virtual keyboard for text areas

--- projects/player/src/app/components/keyboard/keyboard.component.ts
+++ projects/player/src/app/components/keyboard/keyboard.component.ts
@@ -8,6 +8,7 @@ import { Component, Input } from '@angular/core';
 export class KeyboardComponent {
   @Input() preset!: 'french' | 'numbers' | 'numbersAndOperators' | 'comparisonOperators' | 'none';
   @Input() inputComponent!: HTMLTextAreaElement | HTMLInputElement;
+  @Input() inputType!: 'text-field' | 'text-area';
 
   onMouseDown = (event: MouseEvent, stopPropagation: boolean): void => {
     event.preventDefault();

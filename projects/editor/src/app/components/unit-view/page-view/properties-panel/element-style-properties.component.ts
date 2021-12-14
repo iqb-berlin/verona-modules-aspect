d67ba7511abe398abd63f28d8fce8ff6290c9b36
rhenck <richard.henck@iqb.hu-berlin.de>
2021-12-14 19:21:43 +0100

    [editor] Refactor color inputs
    Use a suffix button to activate color picker instead of having an extra 
    form field.
    
    Also add direct input for likert line coloring color.

--- projects/editor/src/app/components/unit-view/page-view/properties-panel/element-style-properties.component.ts
+++ projects/editor/src/app/components/unit-view/page-view/properties-panel/element-style-properties.component.ts
@@ -7,17 +7,17 @@ import { UIElement } from '../../../../../../../common/models/uI-element';
   selector: 'app-element-style-properties',
   template: `
     <div fxLayout="column">
-      <mat-form-field *ngIf="combinedProperties.selectionColor !== undefined"
-                      appearance="fill" class="mdInput textsingleline">
-        <mat-label>{{'propertiesPanel.selectionColor' | translate }}</mat-label>
-        <input matInput type="color" [value]="combinedProperties.selectionColor"
-               (input)="updateModel.emit({ property: 'selectionColor', value: $any($event.target).value })">
-      </mat-form-field>
       <mat-form-field *ngIf="combinedProperties.selectionColor !== undefined" appearance="fill">
         <mat-label>{{'propertiesPanel.selectionColor' | translate }}</mat-label>
         <input matInput type="text" [value]="combinedProperties.selectionColor"
                (input)="updateModel.emit({ property: 'selectionColor', value: $any($event.target).value })">
+        <button mat-icon-button matSuffix (click)="selectionColorInput.click()">
+          <mat-icon>edit</mat-icon>
+        </button>
       </mat-form-field>
+      <input matInput type="color" hidden #selectionColorInput
+             [value]="combinedProperties.selectionColor"
+             (input)="updateModel.emit({ property: 'selectionColor', value: $any($event.target).value })">
 
       <mat-form-field *ngIf="combinedProperties.borderRadius !== undefined" appearance="fill">
         <mat-label>{{'propertiesPanel.borderRadius' | translate }}</mat-label>
@@ -28,41 +28,54 @@ import { UIElement } from '../../../../../../../common/models/uI-element';
       <mat-form-field *ngIf="combinedProperties.itemBackgroundColor !== undefined"
                       appearance="fill" class="mdInput textsingleline">
         <mat-label>{{'propertiesPanel.itemBackgroundColor' | translate }}</mat-label>
-        <input matInput type="color" [value]="combinedProperties.itemBackgroundColor"
-               (input)="updateModel.emit({ property: 'itemBackgroundColor', value: $any($event.target).value })">
-      </mat-form-field>
-      <mat-form-field *ngIf="combinedProperties.itemBackgroundColor !== undefined"
-                      appearance="fill" class="mdInput textsingleline">
-        <mat-label>{{'propertiesPanel.itemBackgroundColor' | translate }}</mat-label>
         <input matInput type="text" [value]="combinedProperties.itemBackgroundColor"
                (input)="updateModel.emit({ property: 'itemBackgroundColor', value: $any($event.target).value })">
+        <button mat-icon-button matSuffix (click)="itembackgroundColorInput.click()">
+          <mat-icon>edit</mat-icon>
+        </button>
       </mat-form-field>
+      <input matInput type="color" hidden #itembackgroundColorInput
+             [value]="combinedProperties.itemBackgroundColor"
+             (input)="updateModel.emit({ property: 'itemBackgroundColor', value: $any($event.target).value })">
 
-      <mat-form-field *ngIf="combinedProperties.backgroundColor !== undefined"
-                      appearance="fill" class="mdInput textsingleline">
-        <mat-label>{{'propertiesPanel.backgroundColor' | translate }}</mat-label>
-        <input matInput type="color" [value]="combinedProperties.backgroundColor"
-               (input)="updateModel.emit({ property: 'backgroundColor', value: $any($event.target).value })">
-      </mat-form-field>
-      <mat-form-field *ngIf="combinedProperties.backgroundColor !== undefined"
+    <mat-form-field *ngIf="combinedProperties.backgroundColor !== undefined"
                       appearance="fill" class="mdInput textsingleline">
         <mat-label>{{'propertiesPanel.backgroundColor' | translate }}</mat-label>
         <input matInput type="text" [value]="combinedProperties.backgroundColor"
                (input)="updateModel.emit({ property: 'backgroundColor', value: $any($event.target).value })">
+        <button mat-icon-button matSuffix (click)="backgroundColorInput.click()">
+          <mat-icon>edit</mat-icon>
+        </button>
       </mat-form-field>
+      <input matInput type="color" hidden #backgroundColorInput
+             [value]="combinedProperties.backgroundColor"
+             (input)="updateModel.emit({ property: 'backgroundColor', value: $any($event.target).value })">
 
-      <mat-form-field *ngIf="combinedProperties.fontColor !== undefined"
+      <mat-form-field *ngIf="combinedProperties.borderColor !== undefined"
                       appearance="fill" class="mdInput textsingleline">
-        <mat-label>{{'propertiesPanel.fontColor' | translate }}</mat-label>
-        <input matInput type="color" [value]="combinedProperties.fontColor"
-               (input)="updateModel.emit({ property: 'fontColor', value: $any($event.target).value })">
+        <mat-label>{{'propertiesPanel.borderColor' | translate }}</mat-label>
+        <input matInput type="text" [value]="combinedProperties.borderColor"
+               (input)="updateModel.emit({ property: 'borderColor', value: $any($event.target).value })">
+        <button mat-icon-button matSuffix (click)="borderColorInput.click()">
+          <mat-icon>edit</mat-icon>
+        </button>
       </mat-form-field>
+      <input matInput type="color" hidden #borderColorInput
+             [value]="combinedProperties.borderColor"
+             (input)="updateModel.emit({ property: 'borderColor', value: $any($event.target).value })">
+
       <mat-form-field *ngIf="combinedProperties.fontColor !== undefined"
                       appearance="fill" class="mdInput textsingleline">
         <mat-label>{{'propertiesPanel.fontColor' | translate }}</mat-label>
         <input matInput type="text" [value]="combinedProperties.fontColor"
                (input)="updateModel.emit({ property: 'fontColor', value: $any($event.target).value })">
+        <button mat-icon-button matSuffix (click)="fontColorInput.click()">
+          <mat-icon>edit</mat-icon>
+        </button>
       </mat-form-field>
+      <input matInput type="color" hidden #fontColorInput
+             [value]="combinedProperties.fontColor"
+             (input)="updateModel.emit({ property: 'fontColor', value: $any($event.target).value })">
 
       <mat-form-field *ngIf="combinedProperties.font !== undefined"
                       appearance="fill" class="mdInput textsingleline">

dbbca80b8378ef53f1f9868b3f48b80201ae3237
jojohoch <joachim.hoch@iqb.hu-berlin.de>
2022-02-02 09:19:10 +0100

    Add a Unicode font to display squares, bars, and dots

--- projects/player/src/app/components/key/key.component.ts
+++ projects/player/src/app/components/key/key.component.ts
@@ -1,6 +1,7 @@
 import {
   Component, EventEmitter, Input, Output
 } from '@angular/core';
+import { InputAssistancePreset } from '../../../../../common/models/uI-element';
 
 @Component({
   selector: 'app-key',
@@ -12,6 +13,7 @@ export class KeyComponent {
   @Input() oval!: boolean;
   @Input() position!: 'floating' | 'right';
   @Input() last!: boolean;
+  @Input() preset!: InputAssistancePreset;
 
   @Output() enterKey = new EventEmitter<string>();
 }

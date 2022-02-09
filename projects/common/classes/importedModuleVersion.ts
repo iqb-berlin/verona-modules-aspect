0a0afa255c468b5d3adb6c353461867d889c210b
rhenck <richard.henck@iqb.hu-berlin.de>
2022-02-09 10:59:07 +0100

    Refactor imported unit version to not rely on semver package
    The package was not a proper ECMA Script package and therefore can not 
    be tree shaken properly and would unnecessarily increase the package 
    size.

--- projects/common/classes/importedModuleVersion.ts
+++ projects/common/classes/importedModuleVersion.ts
@@ -1,62 +1,54 @@
-export class ImportModuleVersion {
-  private static unitLoaded: boolean;
-  private static version: string;
+export class ImportedModuleVersion {
+  static unitLoaded: boolean;
+  static version: string | null = null; // null for invalid
 
-  static setVersion(importedModuleVersion: string): void {
-    ImportModuleVersion.unitLoaded = false;
-    ImportModuleVersion.version = importedModuleVersion;
-  }
-
-  static setUnitLoaded(): void {
-    ImportModuleVersion.unitLoaded = true;
-  }
-
-  static isUnitLoaded(): boolean {
-    return ImportModuleVersion.unitLoaded;
-  }
-
-  static getVersion(): { fullName: string, type: string, version: number[] } {
-    return {
-      fullName: ImportModuleVersion.version,
-      type: ImportModuleVersion.getVersionType(),
-      version: ImportModuleVersion.getVersionNumbers()
-    };
-  }
-
-  private static getVersionType(): string {
-    if (ImportModuleVersion.version && ImportModuleVersion.version.length) {
-      const importedModuleVersionArray = ImportModuleVersion.version.split('@');
-      if (importedModuleVersionArray.length === 2) {
-        return importedModuleVersionArray[0];
-      }
+  static setVersion(importedModuleVersion: string | undefined): void {
+    ImportedModuleVersion.unitLoaded = false;
+    if (importedModuleVersion) {
+      ImportedModuleVersion.verifyVersionString(importedModuleVersion) ?
+        ImportedModuleVersion.version = importedModuleVersion.split('@')[1] :
+        ImportedModuleVersion.version = null;
     }
-    return 'none';
   }
 
-  private static getVersionNumbers(): number[] {
-    if (ImportModuleVersion.version && ImportModuleVersion.version.length) {
-      const importedModuleVersionArray = ImportModuleVersion.version.split('@');
-      if (importedModuleVersionArray.length === 2) {
-        const versionArray = importedModuleVersionArray[1].split('.');
-        if (versionArray.length === 3) {
-          return versionArray.map(number => Number(number));
-        }
-      }
-    }
-    return [0, 0, 0];
-  }
-
-  static verifyVersion(): boolean {
-    if (!ImportModuleVersion.getVersion().fullName) {
+  /*
+  Checks for existence of parts before and after the @ sign. The first part has to be
+  'iqb-aspect-definition', which indicates a known and supported version.
+  The second part has to have exactly 3 numbers, split at the . (dot) sign.
+   */
+  static verifyVersionString(versionString: string): boolean {
+    if (versionString.split('@').length !== 2 ||
+        versionString.split('@')[0] !== 'iqb-aspect-definition' ||
+        versionString.split('@')[1].split('.').length !== 3) {
+      console.error('Error reading the unit definition!');
       return false;
     }
-    if (ImportModuleVersion.getVersionType() !== 'iqb-aspect-definition') {
+    return true;
+  }
+
+  static isGreaterThanOrEqualTo(versionString: string): boolean {
+    if (!ImportedModuleVersion.version) {
       return false;
     }
-    const numbers = ImportModuleVersion.getVersionNumbers();
-    if (numbers[0] < 1) {
+
+    if (ImportedModuleVersion.version[0] < versionString[0]) {
       return false;
     }
-    return numbers[1] >= 1;
+    if (ImportedModuleVersion.version[0] > versionString[0]) {
+      return true;
+    }
+
+    if (ImportedModuleVersion.version[1] < versionString[1]) {
+      return false;
+    }
+    if (ImportedModuleVersion.version[1] > versionString[1]) {
+      return true;
+    }
+
+    if (ImportedModuleVersion.version[2] < versionString[2]) {
+      return false;
+    }
+
+    return true;
   }
 }

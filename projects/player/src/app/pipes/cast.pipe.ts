a285a9e4c9a86706dd89b8090c91a6293d04c29e
jojohoch <joachim.hoch@iqb.hu-berlin.de>
2022-03-03 08:59:57 +0100

    [player] Convert `transform` function of CastPipe to arrow function

--- projects/player/src/app/pipes/cast.pipe.ts
+++ projects/player/src/app/pipes/cast.pipe.ts
@@ -4,14 +4,5 @@ import { Pipe, PipeTransform } from '@angular/core';
   name: 'cast'
 })
 export class CastPipe implements PipeTransform {
-  /**
-   * Cast (S: SuperType) into (T: Type) using @Generics.
-   * @param value (S: SuperType) obtained from input type.
-   * @optional @param type (T CastingType)
-   * type?: { new (): T }
-   * type?: new () => T
-   */
-  transform<S, T extends S>(value: S, type?: T): T {
-    return <T>value;
-  }
+  transform = <S, T extends S>(value: S, type: T): T => <T>value;
 }

259635c26598459b19aee1e67f0b39f3276519f2
jojohoch <joachim.hoch@iqb.hu-berlin.de>
2022-03-04 10:17:27 +0100

    [player] Use function notation for transform method in cast pipe
    to avoid lint errors in templates

--- projects/player/src/app/pipes/cast.pipe.ts
+++ projects/player/src/app/pipes/cast.pipe.ts
@@ -4,5 +4,8 @@ import { Pipe, PipeTransform } from '@angular/core';
   name: 'cast'
 })
 export class CastPipe implements PipeTransform {
-  transform = <S, T extends S>(value: S, type: T): T => <T>value;
+  // eslint-disable-next-line class-methods-use-this
+  transform<S, T extends S>(value: S, type: T): T {
+    return <T>value;
+  }
 }

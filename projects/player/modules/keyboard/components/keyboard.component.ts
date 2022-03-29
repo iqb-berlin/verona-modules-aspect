638ab63b55b753d46aec7ca8ab3627f732623919
rhenck <richard.henck@iqb.hu-berlin.de>
2022-03-29 13:51:35 +0200

    [player] Improve keyboard component to show more keys for special chars
    The number row has alternative keys, activatable via shift.
    
    Also add new keys for dot, comma etc.

--- projects/player/modules/keyboard/components/keyboard.component.ts
+++ projects/player/modules/keyboard/components/keyboard.component.ts
@@ -15,9 +15,17 @@ export class KeyboardComponent {
   @Output() backspaceClicked = new EventEmitter();
 
   shift = false;
+  numberKeys: [string, string][] = [['1', '!'], ['2', '"'], ['3', '§'], ['4', '$'], ['5', '%'], ['6', '&'],
+    ['7', '/'], ['8', '('], ['9', ')'], ['0', '='], ['ß', '?']];
+
+  frenchSpecialCharacters = ['â', 'à', 'æ', 'ê', 'è', 'é', 'ë', 'î', 'ï', 'ô', 'ò', 'œ', 'û', 'ù', 'ü', 'ç'];
 
   enterCharacter(pressedCharacter: string): void {
     this.characterClicked.emit(this.shift ? pressedCharacter.toUpperCase() : pressedCharacter);
+  }
+
+  enterAltCharacter(pressedCharacter: [string, string]): void {
+    this.characterClicked.emit(this.shift ? pressedCharacter[1] : pressedCharacter[0]);
   }
 
   toggleShift(): void {

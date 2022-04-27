b7df46b4b11df10484bc55649f4722aa3ad3459a
jojohoch <joachim.hoch@iqb.hu-berlin.de>
2022-04-27 08:27:31 +0200

    [player] Enable logs in standalone production mode

--- projects/player/src/main.ts
+++ projects/player/src/main.ts
@@ -6,7 +6,7 @@ import { environment } from './environments/environment';
 
 if (environment.production) {
   enableProdMode();
-  if (window) {
+  if (window && window !== window.parent) {
     window.console.log = () => {};
   }
 }

b9b7b4e500248af6738c122b5c5388b271bcd56d
jojohoch <joachim.hoch@iqb.hu-berlin.de>
2022-04-28 15:13:39 +0200

    [player] Add tests for ValidationService

--- projects/player/src/app/services/validation.service.spec.ts
+++ projects/player/src/app/services/validation.service.spec.ts
@@ -1,5 +1,6 @@
 import { TestBed } from '@angular/core/testing';
 import { ValidationService } from './validation.service';
+import { FormControl } from '@angular/forms';
 
 describe('ValidationService', () => {
   let service: ValidationService;
@@ -11,4 +12,25 @@ describe('ValidationService', () => {
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  it('responseProgress should be complete', () => {
+    expect(service.responseProgress).toEqual('complete');
+  });
+
+  it('responseProgress should be complete', () => {
+    service.registerFormControl(new FormControl('TEST'));
+    expect(service.responseProgress).toEqual('complete');
+  });
+
+  it('responseProgress should be none', () => {
+    service.registerFormControl(new FormControl(''));
+    expect(service.responseProgress).toEqual('none');
+  });
+
+  it('responseProgress should be some', () => {
+    service.registerFormControl(new FormControl('TEST'));
+    service.registerFormControl(new FormControl(''));
+    expect(service.responseProgress).toEqual('some');
+  });
+
 });

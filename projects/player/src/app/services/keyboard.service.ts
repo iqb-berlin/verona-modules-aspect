9405524c4d9703647f2bf4545b9e16b337601a56
jojohoch <joachim.hoch@iqb.hu-berlin.de>
2022-05-02 10:37:47 +0200

    [player] Remove unnecessary casting of elementModel in KeyboardService
    Remove unused property alternativeKeyboard

--- projects/player/src/app/services/keyboard.service.ts
+++ projects/player/src/app/services/keyboard.service.ts
@@ -2,23 +2,19 @@ import { Injectable } from '@angular/core';
 import { InputService } from '../classes/input-service';
 import { TextAreaComponent } from 'common/components/ui-elements/text-area.component';
 import { TextFieldComponent } from 'common/components/ui-elements/text-field.component';
-import { TextFieldElement } from 'common/interfaces/elements';
 import { SpellCorrectComponent } from 'common/components/ui-elements/spell-correct.component';
 
 @Injectable({
   providedIn: 'root'
 })
 export class KeyboardService extends InputService {
-  alternativeKeyboard!: boolean;
   alternativeKeyboardShowFrench!: boolean;
 
   toggle(focusedElement: HTMLElement | null,
          elementComponent: TextAreaComponent | TextFieldComponent | SpellCorrectComponent,
          isMobileWithoutHardwareKeyboard: boolean): boolean {
     if (focusedElement && isMobileWithoutHardwareKeyboard) {
-      this.alternativeKeyboard = (elementComponent.elementModel as TextFieldElement).showSoftwareKeyboard;
-      this.alternativeKeyboardShowFrench =
-        (elementComponent.elementModel as TextFieldElement).softwareKeyboardShowFrench;
+      this.alternativeKeyboardShowFrench = elementComponent.elementModel.softwareKeyboardShowFrench;
       this.scrollElement(focusedElement);
       this.isOpen = this.open(focusedElement, elementComponent);
     } else {

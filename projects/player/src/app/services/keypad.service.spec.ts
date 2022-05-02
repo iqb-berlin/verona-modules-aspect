1639afed9a9194ac9b6ed14f6ac7b5713639f5cf
jojohoch <joachim.hoch@iqb.hu-berlin.de>
2022-05-02 18:45:35 +0200

    [player] Add keypad tests for TextFieldSimpleElements

--- projects/player/src/app/services/keypad.service.spec.ts
+++ projects/player/src/app/services/keypad.service.spec.ts
@@ -1,27 +1,30 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { FormControl } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { KeyInputModule } from 'key-input/key-input.module';
 import { KeypadService } from './keypad.service';
 import { TextFieldComponent } from 'common/components/ui-elements/text-field.component';
 import { TextAreaComponent } from 'common/components/ui-elements/text-area.component';
+import { TextFieldSimpleComponent } from 'common/components/ui-elements/text-field-simple.component';
 import { SpellCorrectComponent } from 'common/components/ui-elements/spell-correct.component';
 
 import * as textField_130 from 'test-data/element-models/text-field_130.json';
+import * as textFieldSimple_131 from 'test-data/element-models/text-field-simple_131.json';
 import * as textArea_130 from 'test-data/element-models/text-area_130.json';
 import * as spellCorrect_130 from 'test-data/element-models/spell-correct_130.json';
+
 
 describe('KeypadService', () => {
   let service: KeypadService;
   let textFieldComponentFixture: ComponentFixture<TextFieldComponent>;
   let textFieldComponent: TextFieldComponent;
+  let textFieldSimpleComponentFixture: ComponentFixture<TextFieldSimpleComponent>;
+  let textFieldSimpleComponent: TextFieldSimpleComponent;
   let textAreaComponentFixture: ComponentFixture<TextAreaComponent>;
   let textAreaComponent: TextAreaComponent;
   let spellCorrectComponentFixture: ComponentFixture<SpellCorrectComponent>;
   let spellCorrectComponent: SpellCorrectComponent;
   beforeEach(() => {
     TestBed.configureTestingModule({
-      declarations: [TextFieldComponent, TextAreaComponent],
       imports: [
         BrowserAnimationsModule,
         KeyInputModule
@@ -32,19 +35,21 @@ describe('KeypadService', () => {
     textFieldComponentFixture = TestBed.createComponent(TextFieldComponent);
     textFieldComponent = textFieldComponentFixture.componentInstance;
     textFieldComponent.elementModel = JSON.parse(JSON.stringify(textField_130));
-    textFieldComponent.elementFormControl = new FormControl('');
+
+    textFieldSimpleComponentFixture = TestBed.createComponent(TextFieldSimpleComponent);
+    textFieldSimpleComponent = textFieldSimpleComponentFixture.componentInstance;
+    textFieldSimpleComponent.elementModel = JSON.parse(JSON.stringify(textFieldSimple_131));
 
     textAreaComponentFixture = TestBed.createComponent(TextAreaComponent);
     textAreaComponent = textAreaComponentFixture.componentInstance;
     textAreaComponent.elementModel = JSON.parse(JSON.stringify(textArea_130));
-    textAreaComponent.elementFormControl = new FormControl('');
 
     spellCorrectComponentFixture = TestBed.createComponent(SpellCorrectComponent);
     spellCorrectComponent = spellCorrectComponentFixture.componentInstance;
     spellCorrectComponent.elementModel = JSON.parse(JSON.stringify(spellCorrect_130));
-    spellCorrectComponent.elementFormControl = new FormControl('');
 
     textFieldComponentFixture.detectChanges();
+    textFieldSimpleComponentFixture.detectChanges();
     textAreaComponentFixture.detectChanges();
     spellCorrectComponentFixture.detectChanges();
   });
@@ -156,6 +161,111 @@ describe('KeypadService', () => {
     service.toggle(input, textFieldComponent);
     expect(service.position).toEqual('floating');
   });
+
+  // textFieldSimple
+
+  it('should toggle keypad to open', () => {
+    const input = textFieldSimpleComponent.domElement.querySelector('input') as HTMLInputElement;
+    service.toggle(input, textFieldSimpleComponent);
+    expect(service.isOpen).toBeTruthy();
+  });
+
+  it('should toggle keypad to open', () => {
+    const input = textFieldSimpleComponent.domElement.querySelector('input') as HTMLInputElement;
+    expect(service.toggle(input, textFieldSimpleComponent)).toBeTruthy();
+  });
+
+  it('should toggle keypad to close', () => {
+    service.isOpen = true;
+    service.toggle(null, textFieldSimpleComponent);
+    expect(service.isOpen).toBeFalse();
+  });
+
+  it('should toggle keypad to close', () => {
+    service.isOpen = true;
+    expect(service.toggle(null, textFieldSimpleComponent)).toBeFalse();
+  });
+
+  it('should set the inputElement of the service', () => {
+    const input = textFieldSimpleComponent.domElement.querySelector('input') as HTMLInputElement;
+    service.toggle(input, textFieldSimpleComponent);
+    service.enterKey('n');
+    expect(service.inputElement).toEqual(input);
+  });
+
+  it('enter "n" should set the inputElement.value of the service value to "n"', () => {
+    const input = textFieldSimpleComponent.domElement.querySelector('input') as HTMLInputElement;
+    service.toggle(input, textFieldSimpleComponent);
+    service.enterKey('n');
+    expect(service.inputElement.value).toEqual('n');
+  });
+
+  it('enter "!" should set the inputElement.value of the service value to "n!"', () => {
+    const input = textFieldSimpleComponent.domElement.querySelector('input') as HTMLInputElement;
+    input.value = 'n';
+    input.setSelectionRange(1, 1);
+    service.toggle(input, textFieldSimpleComponent);
+    service.enterKey('!');
+    expect(service.inputElement.value).toEqual('n!');
+  });
+
+  it('enter "!" should replace the inputElement.value of the service with to "!"', () => {
+    const input = textFieldSimpleComponent.domElement.querySelector('input') as HTMLInputElement;
+    input.value = 'n';
+    input.setSelectionRange(0, 1);
+    service.toggle(input, textFieldSimpleComponent);
+    service.enterKey('!');
+    expect(service.inputElement.value).toEqual('!');
+  });
+
+  it('deleterCharacters with parameter true should deleterCharacters backwards', () => {
+    const input = textFieldSimpleComponent.domElement.querySelector('input') as HTMLInputElement;
+    input.value = 'n';
+    input.setSelectionRange(1, 1);
+    service.toggle(input, textFieldSimpleComponent);
+    service.deleterCharacters(true);
+    expect(service.inputElement.value).toEqual('');
+  });
+
+  it('deleterCharacters with parameter false should deleterCharacters forwards', () => {
+    const input = textFieldSimpleComponent.domElement.querySelector('input') as HTMLInputElement;
+    input.value = 'n';
+    input.setSelectionRange(0, 0);
+    service.toggle(input, textFieldSimpleComponent);
+    service.deleterCharacters(false);
+    expect(service.inputElement.value).toEqual('');
+  });
+
+  it('deleterCharacters with parameter true should delete all characters', () => {
+    const input = textFieldSimpleComponent.domElement.querySelector('input') as HTMLInputElement;
+    input.value = 'all values';
+    input.setSelectionRange(0, input.value.length);
+    service.toggle(input, textFieldSimpleComponent);
+    service.deleterCharacters(true);
+    expect(service.inputElement.value).toEqual('');
+  });
+
+  it('deleterCharacters with parameter true should delete all characters', () => {
+    const input = textFieldSimpleComponent.domElement.querySelector('input') as HTMLInputElement;
+    input.value = 'all values';
+    input.setSelectionRange(0, input.value.length);
+    service.toggle(input, textFieldSimpleComponent);
+    service.deleterCharacters(false);
+    expect(service.inputElement.value).toEqual('');
+  });
+
+  it('preset should be set to "french"', () => {
+    const input = textFieldSimpleComponent.domElement.querySelector('input') as HTMLInputElement;
+    service.toggle(input, textFieldSimpleComponent);
+    expect(service.preset).toEqual('french');
+  });
+
+  it('position should be set to "floating"', () => {
+    const input = textFieldSimpleComponent.domElement.querySelector('input') as HTMLInputElement;
+    service.toggle(input, textFieldSimpleComponent);
+    expect(service.position).toEqual('floating');
+  });
+
 
   // textArea
 

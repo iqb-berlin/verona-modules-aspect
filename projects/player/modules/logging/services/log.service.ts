5e12d33668875de51c66ca5214113fc3ce3f0e84
jojohoch <joachim.hoch@iqb.hu-berlin.de>
2022-05-06 18:12:56 +0200

    [player] Set log level to 1 and rename enum LogLevel
    - Disable explicit any rule, because here the native interface of
    'window.console' uses 'any[]'

--- projects/player/modules/logging/services/log.service.ts
+++ projects/player/modules/logging/services/log.service.ts
@@ -8,24 +8,28 @@ export enum LogLevel { LOG = 0, INFO = 1, WARN = 2, ERROR = 3, NONE = 4}
 export class LogService {
   static level: LogLevel = 1;
 
+  // eslint-disable-next-line  @typescript-eslint/no-explicit-any
   static error(...args: any[]): void {
     if (LogService.level >= LogLevel.ERROR) {
       window.console.error.apply( console, args );
     }
   }
 
+  // eslint-disable-next-line  @typescript-eslint/no-explicit-any
   static warn(...args: any[]): void {
     if (LogService.level >= LogLevel.WARN) {
       window.console.warn.apply( console, args );
     }
   }
 
+  // eslint-disable-next-line  @typescript-eslint/no-explicit-any
   static info(...args: any[]): void {
     if (LogService.level >= LogLevel.INFO) {
       window.console.info.apply( console, args );
     }
   }
 
+  // eslint-disable-next-line  @typescript-eslint/no-explicit-any
   static log(...args: any[]): void {
     if (LogService.level >= LogLevel.LOG) {
       window.console.info.apply( console, args );

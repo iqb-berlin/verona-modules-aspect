0c9b6201b8d771f49b46f28b66dc02e6c96c692b
jojohoch <joachim.hoch@iqb.hu-berlin.de>
2022-05-09 17:01:51 +0200

    Improve sanitizing drop-list-simple children of cloze elements

--- projects/common/services/sanitization.service.ts
+++ projects/common/services/sanitization.service.ts
@@ -242,8 +242,9 @@ export class SanitizationService {
     } else {
       childElements = (element.parts as any[])
         .map((el: any) => el
-          .filter((el2: { type: string; }) => ['text-field', 'text-field-simple', 'drop-list', 'drop-list', 'toggle-button']
-            .includes(el2.type)).value)
+          .filter((el2: { type: string; }) => [
+            'text-field', 'text-field-simple', 'drop-list', 'drop-list-simple', 'toggle-button'
+          ].includes(el2.type)).value)
         .flat();
       doc = SanitizationService.createClozeDocument(element);
     }

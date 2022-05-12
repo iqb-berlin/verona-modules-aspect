5aab62ef7775ebe69a08688aa54b3b2fbd88a016
jojohoch <joachim.hoch@iqb.hu-berlin.de>
2022-05-12 19:08:38 +0200

    [player] Fix basic test for SectionContainerComponent

--- projects/player/src/app/components/section-container/section-container.component.spec.ts
+++ projects/player/src/app/components/section-container/section-container.component.spec.ts
@@ -1,5 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { SectionContainerComponent } from './section-container.component';
+import { SectionComponent } from 'player/src/app/components/section/section.component';
 
 describe('SectionContainerComponent', () => {
   let component: SectionContainerComponent;
@@ -8,7 +9,8 @@ describe('SectionContainerComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [
-        SectionContainerComponent
+        SectionContainerComponent,
+        SectionComponent
       ]
     })
       .compileComponents();
@@ -17,6 +19,18 @@ describe('SectionContainerComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(SectionContainerComponent);
     component = fixture.componentInstance;
+    component.pageSections = [];
+    component.section = {
+      elements: [],
+      height: 400,
+      backgroundColor: '#ffffff',
+      dynamicPositioning: true,
+      autoColumnSize: true,
+      autoRowSize: true,
+      gridColumnSizes: '1fr 1fr',
+      gridRowSizes: '1fr',
+      activeAfterID: ''
+    };
     fixture.detectChanges();
   });
 

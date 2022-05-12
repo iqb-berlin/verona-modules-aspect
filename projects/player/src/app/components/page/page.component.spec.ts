40ddbcd7eda63dafbd81dff14e9c4b38a7beac6f
jojohoch <joachim.hoch@iqb.hu-berlin.de>
2022-05-12 14:40:59 +0200

    [player] Fix typo in test of PageComponent

--- projects/player/src/app/components/page/page.component.spec.ts
+++ projects/player/src/app/components/page/page.component.spec.ts
@@ -1,7 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { PageComponent } from './page.component';
 
-describe('PlayerStateComponent', () => {
+describe('PageComponent', () => {
   let component: PageComponent;
   let fixture: ComponentFixture<PageComponent>;
 

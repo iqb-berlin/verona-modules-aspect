ed8c5f6e3e5a10f65482ad650ebe7fa14388e063
jojohoch <joachim.hoch@iqb.hu-berlin.de>
2022-05-13 09:52:29 +0200

    [player] Add basic test for ElementCompoundGroupComponent

---
+++ projects/player/src/app/components/element-compound-group/element-compound-group.component.spec.ts
@@ -0,0 +1,65 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import {
+  ElementCompoundGroupComponent
+} from './element-compound-group.component';
+import { TranslateLoader, TranslateModule, TranslateService } from '@ngx-translate/core';
+import { PlayerTranslateLoader } from 'player/src/app/classes/player-translate-loader';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { FloatingKeypadComponent } from 'player/src/app/components/floating-keypad/floating-keypad.component';
+import { Component, Input } from '@angular/core';
+import { UIElement } from 'common/interfaces/elements';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { CastPipe } from 'player/src/app/pipes/cast.pipe';
+
+
+describe('ElementCompoundGroupComponent', () => {
+  let component: ElementCompoundGroupComponent;
+  let fixture: ComponentFixture<ElementCompoundGroupComponent>;
+
+  @Component({ selector: 'aspect-likert', template: '' })
+  class LikertStubComponent {
+    @Input() elementModel!: UIElement;
+  }
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [
+        ElementCompoundGroupComponent,
+        FloatingKeypadComponent,
+        LikertStubComponent,
+        CastPipe
+      ],
+      imports: [
+        FormsModule,
+        ReactiveFormsModule,
+        MatSnackBarModule,
+        TranslateModule.forRoot({
+          loader: {
+            provide: TranslateLoader,
+            useClass: PlayerTranslateLoader
+          }
+        })
+      ],
+      providers: [TranslateService]
+    })
+      .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(ElementCompoundGroupComponent);
+    component = fixture.componentInstance;
+    component.elementModel = {
+      type: 'likert',
+      id: 'test',
+      width: 0,
+      height: 0,
+      rows: []
+    };
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+
+});

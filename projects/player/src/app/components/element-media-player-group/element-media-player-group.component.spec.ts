f183c1ba4c0340b546b1e8ddb895735af84fa579
jojohoch <joachim.hoch@iqb.hu-berlin.de>
2022-05-13 14:31:50 +0200

    [player] Add basic test for ElementMediaPlayerGroupComponent

---
+++ projects/player/src/app/components/element-media-player-group/element-media-player-group.component.spec.ts
@@ -0,0 +1,57 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ElementMediaPlayerGroupComponent } from './element-media-player-group.component';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { UIElement, ValueChangeElement } from 'common/interfaces/elements';
+import { CastPipe } from 'player/src/app/pipes/cast.pipe';
+import { UnitStateService } from 'player/src/app/services/unit-state.service';
+import { Subject } from 'rxjs';
+
+describe('ElementMediaPlayerGroupComponent', () => {
+  let component: ElementMediaPlayerGroupComponent;
+  let fixture: ComponentFixture<ElementMediaPlayerGroupComponent>;
+  let mockUnitStateService: UnitStateService;
+
+  @Component({ selector: 'aspect-audio', template: '' })
+  class AudioStubComponent {
+    @Input() elementModel!: UIElement;
+    @Input() savedPlaybackTime!: number;
+    @Input() actualPlayingId!: Subject<string | null>;
+    @Input() mediaStatusChanged!: Subject<string>;
+    @Output() elementValueChanged = new EventEmitter<ValueChangeElement>();
+    @Output() onMediaPlayStatusChanged = new EventEmitter<string | null>();
+    @Output() onMediaValidStatusChanged = new EventEmitter<string>();
+  }
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [
+        ElementMediaPlayerGroupComponent,
+        AudioStubComponent,
+        CastPipe
+      ]
+    })
+      .compileComponents();
+  });
+
+  beforeEach(() => {
+    mockUnitStateService = TestBed.inject(UnitStateService);
+    fixture = TestBed.createComponent(ElementMediaPlayerGroupComponent);
+    spyOn(mockUnitStateService, 'registerElement')
+      .withArgs('test', 0, document.createElement('div'), 1);
+    spyOn(mockUnitStateService, 'getElementCodeById').withArgs('test').and
+      .returnValue({ id: 'test', status: 'NOT_REACHED', value: 0 });
+    component = fixture.componentInstance;
+    component.elementModel = {
+      type: 'audio',
+      id: 'test',
+      width: 0,
+      height: 0
+    };
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+
+});

0808a480b8ce035ec22bd93790b3184cf184120f
jojohoch <joachim.hoch@iqb.hu-berlin.de>
2022-05-16 11:04:36 +0200

    [player] Add tests for PageLabelDirective

---
+++ projects/player/src/app/directives/page-label.directive.spec.ts
@@ -0,0 +1,51 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { PageLabelDirective } from 'player/src/app/directives/page-label.directive';
+import { Component, Input } from '@angular/core';
+import { By } from '@angular/platform-browser';
+
+describe('PageLabelDirective', () => {
+  @Component({
+    template: `
+    <div id="container"
+         aspectPageLabel
+         [isHidden]="headerIsHidden"
+         (getHeight)="headerHeight=$event">
+      <div id="header"
+           [style.height.px]="100">FIRST CHILD</div>
+      <div>Content</div>
+    </div>`
+  })
+  class TestComponent {
+    @Input()headerIsHidden = true;
+    headerHeight = 0;
+  }
+
+  let component: TestComponent;
+  let fixture: ComponentFixture<TestComponent>;
+
+  beforeEach(() => {
+    fixture = TestBed.configureTestingModule({
+      declarations: [ TestComponent, PageLabelDirective ]
+    })
+      .createComponent(TestComponent);
+    component = fixture.componentInstance;
+  });
+
+
+  it('should color 1st <h2> background "yellow"', () => {
+    component.headerIsHidden = true;
+    fixture.detectChanges();
+    const header = fixture.debugElement.query(By.css('#header'));
+    expect(header.nativeElement.style.display).toBe('none');
+    expect(component.headerHeight).toBe(0);
+  });
+
+  it('should color 1st <h2> background "yellow"', () => {
+    component.headerIsHidden = false;
+    fixture.detectChanges();
+    const header = fixture.debugElement.query(By.css('#header'));
+    expect(header.nativeElement.style.display).not.toBe('none');
+    expect(component.headerHeight).toBe(100);
+  });
+
+});

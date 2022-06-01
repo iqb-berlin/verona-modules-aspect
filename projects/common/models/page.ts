eb6008f96e9c5aeb530e2785ae12790dabf8e2ea
rhenck <richard.henck@iqb.hu-berlin.de>
2022-06-01 23:38:36 +0200

    Fix page constructor parameter
    Page needs to be a partial as it is only the blueprint.

--- projects/common/models/page.ts
+++ projects/common/models/page.ts
@@ -13,7 +13,7 @@ export class Page {
   alwaysVisiblePagePosition: 'left' | 'right' | 'top' | 'bottom' = 'left';
   alwaysVisibleAspectRatio: number = 50;
 
-  constructor(page?: Page, idManager?: IDManager) {
+  constructor(page?: Partial<Page>, idManager?: IDManager) {
     if (page?.hasMaxWidth) this.hasMaxWidth = page.hasMaxWidth;
     if (page?.maxWidth) this.maxWidth = page.maxWidth;
     if (page?.margin) this.margin = page.margin;
@@ -21,7 +21,7 @@ export class Page {
     if (page?.alwaysVisible) this.alwaysVisible = page.alwaysVisible;
     if (page?.alwaysVisiblePagePosition) this.alwaysVisiblePagePosition = page.alwaysVisiblePagePosition;
     if (page?.alwaysVisibleAspectRatio) this.alwaysVisibleAspectRatio = page.alwaysVisibleAspectRatio;
-    this.sections = page?.sections.map(section => new Section(section, idManager)) || [new Section()];
+    this.sections = page?.sections?.map(section => new Section(section, idManager)) || [new Section()];
   }
 
   getAllElements(elementType?: string): UIElement[] {

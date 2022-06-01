4afa509867d84455d03910cf3ed29af383754efd
rhenck <richard.henck@iqb.hu-berlin.de>
2022-06-01 23:38:57 +0200

    [player] Fix tests to use the new Page class

--- projects/player/src/app/pipes/always-visible-page.pipe.spec.ts
+++ projects/player/src/app/pipes/always-visible-page.pipe.spec.ts
@@ -4,7 +4,7 @@ import { Page } from 'common/models/page';
 
 describe('AlwaysVisiblePagePipe', () => {
 
-  const page: Page = {
+  const page: Page = new Page({
     hasMaxWidth: false,
     maxWidth: 0,
     margin: 0,
@@ -13,13 +13,13 @@ describe('AlwaysVisiblePagePipe', () => {
     alwaysVisiblePagePosition: 'left',
     alwaysVisibleAspectRatio: 50,
     sections: []
-  };
+  });
 
   const pipe = new AlwaysVisiblePagePipe();
 
   it('should transform an array of pages to the always visible page of the array', () => {
-    const pages = [page, page, { ...page, alwaysVisible: true }];
-    expect(pipe.transform(pages)).toEqual({ ...page, alwaysVisible: true });
+    const pages = [page, page, { ...page, alwaysVisible: true } as Page];
+    expect(pipe.transform(pages)).toEqual({ ...page, alwaysVisible: true } as Page);
   });
 
   it('should transform an array of pages without any always visible page to null', () => {

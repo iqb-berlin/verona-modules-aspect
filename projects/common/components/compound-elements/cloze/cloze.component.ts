9aa4acf9a334bd90d9b4aece84b31eb99fefe3e4
rhenck <richard.henck@iqb.hu-berlin.de>
2022-06-09 17:00:48 +0200

    [editor] Fix spaces on sub and sup elements in cloze
    Turns out the space within the template code are doing this.
    
    #270

--- projects/common/components/compound-elements/cloze/cloze.component.ts
+++ projects/common/components/compound-elements/cloze/cloze.component.ts
@@ -144,22 +144,14 @@ import { ClozeElement } from 'common/models/elements/compound-elements/cloze/clo
         <ng-container *ngIf="$any(subPart).type === 'text' &&
                              (!(subPart.marks | markList).includes('superscript')) &&
                              (!(subPart.marks | markList).includes('subscript'))">
-          <span [ngStyle]="subPart.marks | styleMarks">
-            {{subPart.text}}
-          </span>
+          <span [ngStyle]="subPart.marks | styleMarks">{{subPart.text}}</span>
         </ng-container>
-
         <ng-container *ngIf="$any(subPart).type === 'text' && (subPart.marks | markList).includes('superscript')">
-          <sup [ngStyle]="subPart.marks | styleMarks">
-            {{subPart.text}}
-          </sup>
+          <sup [ngStyle]="subPart.marks | styleMarks">{{subPart.text}}</sup>
         </ng-container>
         <ng-container *ngIf="$any(subPart).type === 'text' && (subPart.marks | markList).includes('subscript')">
-          <sub [ngStyle]="subPart.marks | styleMarks">
-            {{subPart.text}}
-          </sub>
+          <sub [ngStyle]="subPart.marks | styleMarks">{{subPart.text}}</sub>
         </ng-container>
-
         <ng-container *ngIf="$any(subPart).type === 'image'">
           <img [src]="subPart.attrs.src" [alt]="subPart.attrs.alt"
                [style.display]="'inline-block'"

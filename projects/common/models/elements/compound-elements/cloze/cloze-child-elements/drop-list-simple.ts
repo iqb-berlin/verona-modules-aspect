1b1a112220d3dc4d8dd40be88d2cde6454a0bb93
rhenck <richard.henck@iqb.hu-berlin.de>
2022-08-09 23:49:49 +0200

    Fix reading of DropListSimple valueIds
    Now checks when creating or updating object if IDs are okay.

--- projects/common/models/elements/compound-elements/cloze/cloze-child-elements/drop-list-simple.ts
+++ projects/common/models/elements/compound-elements/cloze/cloze-child-elements/drop-list-simple.ts
@@ -1,12 +1,14 @@
 import { ElementFactory } from 'common/util/element.factory';
-import { BasicStyles, InputElement } from 'common/models/elements/element';
+import { BasicStyles, DragNDropValueObject, InputElement, UIElementValue } from 'common/models/elements/element';
 import { Type } from '@angular/core';
 import { ElementComponent } from 'common/directives/element-component.directive';
 import {
   DropListSimpleComponent
 } from 'common/components/compound-elements/cloze/cloze-child-elements/drop-list-simple.component';
+import { IDManager } from 'common/util/id-manager';
 
 export class DropListSimpleElement extends InputElement {
+  value: DragNDropValueObject[] = [];
   connectedTo: string[] = [];
   copyOnDrop: boolean = false;
   highlightReceivingDropList: boolean = false;
@@ -17,7 +19,9 @@ export class DropListSimpleElement extends InputElement {
 
   constructor(element: Partial<DropListSimpleElement>, ...args: unknown[]) {
     super({ width: 150, height: 30, ...element }, ...args);
-    this.value = element.value || [];
+    if (element.value) {
+      this.value = DropListSimpleElement.checkAndRepairValueIDs(element.value);
+    }
     if (element.connectedTo) this.connectedTo = element.connectedTo;
     if (element.copyOnDrop) this.copyOnDrop = element.copyOnDrop;
     if (element.highlightReceivingDropList) this.highlightReceivingDropList = element.highlightReceivingDropList;
@@ -28,6 +32,25 @@ export class DropListSimpleElement extends InputElement {
         itemBackgroundColor: '#c9e0e0',
         ...element.styling })
     };
+  }
+
+  setProperty(property: string, value: UIElementValue) {
+    if (property === 'value') {
+      this.value = DropListSimpleElement.checkAndRepairValueIDs(value as DragNDropValueObject[]);
+    } else {
+      super.setProperty(property, value);
+    }
+  }
+
+  private static checkAndRepairValueIDs(valueList: DragNDropValueObject[]): DragNDropValueObject[] {
+    valueList.forEach(valueObject => {
+      if (IDManager.getInstance().isIdAvailable(valueObject.id)) {
+        IDManager.getInstance().addID(valueObject.id);
+      } else {
+        valueObject.id = IDManager.getInstance().getNewID('value');
+      }
+    });
+    return valueList;
   }
 
   getComponentFactory(): Type<ElementComponent> {

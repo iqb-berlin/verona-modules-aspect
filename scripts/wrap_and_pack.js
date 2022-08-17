e8fe9892f68a05e9581aa27d438dea2244fa659b
jojohoch <joachim.hoch@iqb.hu-berlin.de>
2022-08-17 11:08:16 +0200

    Fix wrap and pack build with node
    - Replace asynchronous child processes with synchronous processes
    - Add shortcuts to package.json

--- scripts/wrap_and_pack.js
+++ scripts/wrap_and_pack.js
@@ -1,19 +1,18 @@
 #!/usr/bin/env node
 
+// eslint-disable-next-line @typescript-eslint/no-var-requires
 const fs = require('fs');
-const childProcess = require('child_process');
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const execSync = require('child_process').execSync;
 
 const packageName = process.argv[2];
 const packageVersion = process.argv[3];
 const wrapperPath = process.argv[4];
 
-childProcess.fork('node_modules/iqb-dev-components/src/js_css_packer.js',
-  ['dist', packageName, 'dist']);
-
+execSync(`node node_modules/iqb-dev-components/src/js_css_packer.js dist ${packageName} dist`);
 const fileContent = fs.readFileSync(wrapperPath, 'utf8').toString()
   .replace(/version-placeholder/g, packageVersion);
 fs.writeFileSync('dist/index.html', fileContent, 'utf8');
 
 const targetFileName = `verona-${packageName}-aspect-${packageVersion}.html`;
-childProcess.fork('node_modules/iqb-dev-components/src/distpacker.js',
-['dist', targetFileName, packageName]);
+execSync(`node node_modules/iqb-dev-components/src/distpacker.js dist ${targetFileName} ${packageName}`);

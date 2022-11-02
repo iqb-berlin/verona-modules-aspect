babdb884ca3f93473d8f73ad924dbb91ab8455ab
jojohoch <joachim.hoch@iqb.hu-berlin.de>
2022-11-02 10:32:37 +0100

    [player] Remove unnecessary private method from ScrollPagesPipe

--- projects/player/src/app/pipes/scroll-pages.pipe.ts
+++ projects/player/src/app/pipes/scroll-pages.pipe.ts
@@ -5,12 +5,7 @@ import { Page } from 'common/models/page';
   name: 'scrollPages'
 })
 export class ScrollPagesPipe implements PipeTransform {
-
   transform(pages: Page[]): Page[] {
-    return this.getScrollPages(pages);
-  }
-
-  private getScrollPages(pages: Page[]): Page[] {
     return pages.filter((page: Page): boolean => !page.alwaysVisible);
   }
 }

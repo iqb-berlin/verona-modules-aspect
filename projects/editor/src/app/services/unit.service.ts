c835cb769fa9f02b7fa2824897d08a89b547984d
rhenck <richard.henck@iqb.hu-berlin.de>
2022-11-19 20:19:23 +0100

    [editor] Catch errors on unit def reading
    Usually all errors should be caught by the sanitizer, but if that fails 
    the editor will be in an okay state.

--- projects/editor/src/app/services/unit.service.ts
+++ projects/editor/src/app/services/unit.service.ts
@@ -47,13 +47,18 @@ export class UnitService {
   loadUnitDefinition(unitDefinition: string): void {
     this.idService.reset();
     if (unitDefinition) {
-      const unitDef = JSON.parse(unitDefinition);
-      this.sanitizationService.checkAndRepairIDs(unitDef, this.idService, this.messageService);
-      if (SanitizationService.isUnitDefinitionOutdated(unitDef)) {
-        this.unit = new Unit(this.sanitizationService.sanitizeUnitDefinition(unitDef));
-        this.messageService.showMessage(this.translateService.instant('outdatedUnit'));
-      } else {
-        this.unit = new Unit(unitDef);
+      try {
+        const unitDef = JSON.parse(unitDefinition);
+        this.sanitizationService.checkAndRepairIDs(unitDef, this.idService, this.messageService);
+        if (SanitizationService.isUnitDefinitionOutdated(unitDef)) {
+          this.unit = new Unit(this.sanitizationService.sanitizeUnitDefinition(unitDef));
+          this.messageService.showMessage(this.translateService.instant('outdatedUnit'));
+        } else {
+          this.unit = new Unit(unitDef);
+        }
+      } catch (e) {
+        this.messageService.showError('Unit definition konnte nicht gelesen werden!');
+        this.unit = new Unit();
       }
     } else {
       this.unit = new Unit();

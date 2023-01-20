33b9bd7ec4cde38f895eb7463880c63d91e6d013
jojohoch <joachim.hoch@iqb.hu-berlin.de>
2023-01-20 10:40:46 +0100

    [player] Fix test for PageScrollButtonComponent

--- projects/player/src/app/components/page-scroll-button/page-scroll-button.component.spec.ts
+++ projects/player/src/app/components/page-scroll-button/page-scroll-button.component.spec.ts
@@ -1,14 +1,23 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { Component, Input } from '@angular/core';
+import { Section } from 'common/models/section';
 import { PageScrollButtonComponent } from './page-scroll-button.component';
 
 describe('PageScrollButtonComponent', () => {
   let component: PageScrollButtonComponent;
   let fixture: ComponentFixture<PageScrollButtonComponent>;
 
+  @Component({ selector: 'aspect-section', template: '' })
+  class SectionComponent {
+    @Input() section!: Section;
+    @Input() pageIndex!: number;
+  }
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [PageScrollButtonComponent]
+      declarations: [
+        PageScrollButtonComponent,
+        SectionComponent]
     })
       .compileComponents();
   });

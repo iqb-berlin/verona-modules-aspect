372b857a2f43cac045894b3737520b3c4214263c
jojohoch <joachim.hoch@iqb.hu-berlin.de>
2023-06-28 15:47:20 +0200

    [editor] Change style of add button in StateVariablesDialogComponent
    - Swap the position of dialog buttons

--- projects/editor/src/app/components/dialogs/state-variables-dialog/state-variables-dialog.component.ts
+++ projects/editor/src/app/components/dialogs/state-variables-dialog/state-variables-dialog.component.ts
@@ -3,7 +3,19 @@ import { MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { StateVariable } from 'common/models/state-variable';
 
 @Component({
-  templateUrl: './state-variables-dialog.component.html'
+  templateUrl: './state-variables-dialog.component.html',
+  styles: [`
+    .add-button {
+      width: 100%;
+      height: 25px;
+      background-color: #BABABA;
+    }
+    .add-icon {
+      font-size: 24px;
+      color: white;
+      margin-top: -5px;
+    }
+  `]
 })
 export class StateVariablesDialogComponent {
   stateVariables: StateVariable[];

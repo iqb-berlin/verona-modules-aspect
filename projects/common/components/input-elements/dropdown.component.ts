db1192418919706889edabadbb93413d80351302
rhenck <richard.henck@iqb.hu-berlin.de>
2023-10-17 22:31:52 +0200

    Fix DropDown element
    Was completely broken: not showing at all in Editor, not clickable in 
    Player.
    
    Removed pipe 'aspectInputBackgroundColor'. Replaced by more elegant 
    solution via inherited background-color.

--- projects/common/components/input-elements/dropdown.component.ts
+++ projects/common/components/input-elements/dropdown.component.ts
@@ -5,25 +5,22 @@ import { FormElementComponent } from '../../directives/form-element-component.di
 @Component({
   selector: 'aspect-dropdown',
   template: `
-    <mat-form-field
-            appearance="fill"
-            [style.width.%]="100"
-            [style.height.%]="100"
-            aspectInputBackgroundColor [backgroundColor]="elementModel.styling.backgroundColor">
+    <mat-form-field appearance="fill"
+                    [style.background-color]="elementModel.styling.backgroundColor">
       <mat-label [style.color]="elementModel.styling.fontColor"
                  [style.font-size.px]="elementModel.styling.fontSize"
                  [style.font-weight]="elementModel.styling.bold ? 'bold' : ''"
                  [style.font-style]="elementModel.styling.italic ? 'italic' : ''"
                  [style.text-decoration]="elementModel.styling.underline ? 'underline' : ''">
-        {{$any(elementModel).label}}
+        {{ elementModel.label }}
       </mat-label>
       <mat-select [formControl]="elementFormControl" [value]="elementModel.value">
         <mat-option *ngIf="elementModel.allowUnset" [value]="null"
-                    [style.pointer-events]="elementModel.readOnly ? 'none' : 'unset'"
+                    [class.read-only]="elementModel.readOnly"
                     (click)="$event.preventDefault()">
         </mat-option>
         <mat-option *ngFor="let option of elementModel.options; let i = index" [value]="i"
-                    [style.pointer-events]="elementModel.readOnly ? 'none' : 'unset'">
+                    [class.read-only]="elementModel.readOnly">
           {{option.text}}
         </mat-option>
       </mat-select>
@@ -31,7 +28,13 @@ import { FormElementComponent } from '../../directives/form-element-component.di
         {{elementFormControl.errors | errorTransform: elementModel}}
       </mat-error>
     </mat-form-field>
-  `
+  `,
+  styles: [
+    'mat-form-field {width: 100%; height: 100%;}',
+    'read-only {pointer-events: none;}',
+    ':host ::ng-deep mat-form-field .mat-mdc-text-field-wrapper.mdc-text-field {background-color: inherit !important;}',
+    ':host ::ng-deep mat-form-field .mat-mdc-form-field-subscript-wrapper {background-color: white;}'
+  ]
 })
 export class DropdownComponent extends FormElementComponent {
   @Input() elementModel!: DropdownElement;

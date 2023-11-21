3534c2325b13acf47c53e2a13ed30e0692fd3ea3
jojohoch <joachim.hoch@iqb.hu-berlin.de>
2023-11-21 11:11:56 +0100

    Determine the OS to decide whether it is a mobile device

--- projects/player/src/app/services/device.service.ts
+++ projects/player/src/app/services/device.service.ts
@@ -6,12 +6,14 @@ import { Injectable } from '@angular/core';
 export class DeviceService {
   hasHardwareKeyboard: boolean = false;
   private readonly isTouch!: boolean;
+  private readonly isMobile!: boolean;
 
   constructor() {
     this.isTouch = ('ontouchstart' in window) || (navigator && navigator.maxTouchPoints > 0);
+    this.isMobile = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
   }
 
   get isMobileWithoutHardwareKeyboard(): boolean {
-    return this.isTouch && !this.hasHardwareKeyboard;
+    return this.isMobile && this.isTouch && !this.hasHardwareKeyboard;
   }
 }

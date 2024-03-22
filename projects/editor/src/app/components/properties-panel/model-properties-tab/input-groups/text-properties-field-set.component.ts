0914c751033a0107b1b435b29f04dc273dace1dc
rhenck <richard.henck@iqb.hu-berlin.de>
2024-03-22 16:17:32 +0100

    [editor] Improve styling for text element text property
    Add edit button, so it is clear where to click to edit.

--- projects/editor/src/app/components/properties-panel/model-properties-tab/input-groups/text-properties-field-set.component.ts
+++ projects/editor/src/app/components/properties-panel/model-properties-tab/input-groups/text-properties-field-set.component.ts
@@ -9,12 +9,15 @@ import { SelectionService } from '../../../../services/selection.service';
   selector: 'aspect-text-properties-field-set',
   template: `
     <div *ngIf="combinedProperties.text" class="fx-column-start-stretch">
-      <ng-container>
-        <div class="text-text"
-             [innerHTML]="combinedProperties.text | safeResourceHTML"
-             (click)="showTextEditDialog()">
-        </div>
-      </ng-container>
+      Text
+      <div class="text-text"
+           [innerHTML]="combinedProperties.text | safeResourceHTML">
+      </div>
+      <button mat-fab color="primary" [style.align-self]="'center'" [style.margin-bottom.px]="20"
+              [matTooltip]="'Text bearbeiten'"
+              (click)="showTextEditDialog()">
+        <mat-icon>edit</mat-icon>
+      </button>
       <mat-form-field *ngIf="combinedProperties.columnCount != null"
                       appearance="fill" class="mdInput textsingleline">
         <mat-label>{{'propertiesPanel.columnCount' | translate }}</mat-label>
@@ -55,19 +58,11 @@ import { SelectionService } from '../../../../services/selection.service';
   `,
   styles: [`
     .text-text {
-      min-height: 125px;
-      max-height: 400px;
-      overflow: auto;
-      background-color: rgba(0,0,0,.04);
-      cursor: pointer;
       margin-bottom: 10px;
       padding: 10px;
+      max-height: 200px;
+      overflow: scroll;
     }
-
-    .text-text:hover {
-      background-color: rgba(110, 0, 110, .04);
-    }
-
     .fx-column-start-stretch {
       box-sizing: border-box;
       display: flex;

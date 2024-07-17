bae21e040ced2669d0debb634883a246d7e85711
Richard Henck <richard.henck@iqb.hu-berlin.de>
2024-07-17 17:09:05 +0200

    [editor] Change props panel to work with Geometry's optional dimensions
    Also add controls to non-expert mode.

--- projects/editor/src/app/components/properties-panel/position-properties-tab/input-groups/dimension-field-set.component.ts
+++ projects/editor/src/app/components/properties-panel/position-properties-tab/input-groups/dimension-field-set.component.ts
@@ -13,89 +13,120 @@ import { ElementService } from 'editor/src/app/services/unit-services/element.se
                     .sections[selectionService.selectedSectionIndex].dynamicPositioning ||
                     selectionService.isCompoundChildSelected;
                     else elseBlock">
-        <mat-checkbox #fixedWidth [checked]="$any(dimensions.isWidthFixed)"
-                      (change)="updateDimensionProperty('isWidthFixed', $event.checked)">
-          {{'propertiesPanel.isWidthFixed' | translate }}
-        </mat-checkbox>
-        <mat-form-field appearance="fill">
-          <mat-label>{{'propertiesPanel.width' | translate }}</mat-label>
-          <input matInput type="number" min="0"
-                 [disabled]="!fixedWidth.checked"
-                 [ngModel]="dimensions.width"
-                 (ngModelChange)="updateDimensionProperty('width', $event)"
-                 (change)="dimensions.width = dimensions.width ? dimensions.width : 0">
-        </mat-form-field>
+        <ng-container *ngIf="dimensions.isWidthFixed !== undefined else elseBlockWidth">
+          <mat-checkbox #fixedWidth [checked]="$any(dimensions.isWidthFixed)"
+                        (change)="updateDimensionProperty('isWidthFixed', $event.checked)">
+            {{'propertiesPanel.isWidthFixed' | translate }}
+          </mat-checkbox>
+          <mat-form-field appearance="fill">
+            <mat-label>{{'propertiesPanel.width' | translate }}</mat-label>
+            <input matInput type="number" min="0"
+                   [disabled]="!fixedWidth.checked"
+                   [ngModel]="dimensions.width"
+                   (ngModelChange)="updateDimensionProperty('width', $event)"
+                   (change)="dimensions.width = dimensions.width ? dimensions.width : 0">
+          </mat-form-field>
+        </ng-container>
+        <ng-template #elseBlockWidth>
+          <mat-form-field appearance="fill">
+            <mat-label>{{'propertiesPanel.width' | translate }}</mat-label>
+            <input matInput type="number" min="0"
+                   [ngModel]="dimensions.width"
+                   (ngModelChange)="updateDimensionProperty('width', $event)"
+                   (change)="dimensions.width = dimensions.width ? dimensions.width : 0">
+          </mat-form-field>
+        </ng-template>
 
-        <mat-checkbox #fixedHeight [checked]="$any(dimensions.isHeightFixed)"
-                      (change)="updateDimensionProperty('isHeightFixed', $event.checked)">
-          {{'propertiesPanel.isHeightFixed' | translate }}
-        </mat-checkbox>
-        <mat-form-field>
-          <mat-label>
-            {{'propertiesPanel.height' | translate }}
-          </mat-label>
-          <input matInput type="number" min="0"
-                 [disabled]="!fixedHeight.checked"
-                 [ngModel]="dimensions.height"
-                 (ngModelChange)="updateDimensionProperty('height', $event)"
-                 (change)="dimensions.height = dimensions.height ? dimensions.height : 0">
-        </mat-form-field>
+        <ng-container *ngIf="dimensions.isHeightFixed !== undefined else elseBlockHeight">
+          <mat-checkbox #fixedHeight [checked]="$any(dimensions.isHeightFixed)"
+                        (change)="updateDimensionProperty('isHeightFixed', $event.checked)">
+            {{'propertiesPanel.isHeightFixed' | translate }}
+          </mat-checkbox>
+          <mat-form-field>
+            <mat-label>
+              {{'propertiesPanel.height' | translate }}
+            </mat-label>
+            <input matInput type="number" min="0"
+                   [ngModel]="dimensions.height"
+                   (ngModelChange)="updateDimensionProperty('height', $event)"
+                   (change)="dimensions.height = dimensions.height ? dimensions.height : 0">
+          </mat-form-field>
+        </ng-container>
+        <ng-template #elseBlockHeight>
+          <mat-form-field>
+            <mat-label>
+              {{'propertiesPanel.height' | translate }}
+            </mat-label>
+            <input matInput type="number" min="0"
+                   [ngModel]="dimensions.height"
+                   (ngModelChange)="updateDimensionProperty('height', $event)"
+                   (change)="dimensions.height = dimensions.height ? dimensions.height : 0">
+          </mat-form-field>
+        </ng-template>
 
-        <mat-checkbox #minWidthEnabled [checked]="dimensions.minWidth !== null"
-                      (change)="toggleProperty('minWidth', $event.checked)">
-          {{'propertiesPanel.minWidthEnabled' | translate }}
-        </mat-checkbox>
-        <mat-form-field>
-          <mat-label>
-            {{'propertiesPanel.minWidth' | translate }}
-          </mat-label>
-          <input matInput type="number" min="0"
-                 [disabled]="!minWidthEnabled.checked"
-                 [ngModel]="dimensions.minWidth"
-                 (ngModelChange)="updateDimensionProperty('minWidth', $event)">
-        </mat-form-field>
+        <ng-container *ngIf="dimensions.minWidth !== undefined">
+          <mat-checkbox #minWidthEnabled [checked]="dimensions.minWidth !== null"
+                        (change)="toggleProperty('minWidth', $event.checked)">
+            {{'propertiesPanel.minWidthEnabled' | translate }}
+          </mat-checkbox>
+          <mat-form-field>
+            <mat-label>
+              {{'propertiesPanel.minWidth' | translate }}
+            </mat-label>
+            <input matInput type="number" min="0"
+                   [disabled]="!minWidthEnabled.checked"
+                   [ngModel]="dimensions.minWidth"
+                   (ngModelChange)="updateDimensionProperty('minWidth', $event)">
+          </mat-form-field>
+        </ng-container>
 
-        <mat-checkbox #maxWidthEnabled [checked]="dimensions.maxWidth !== null"
-                      (change)="toggleProperty('maxWidth', $event.checked)">
-          {{'propertiesPanel.maxWidthEnabled' | translate }}
-        </mat-checkbox>
-        <mat-form-field>
-          <mat-label>
-            {{'propertiesPanel.maxWidth' | translate }}
-          </mat-label>
-          <input matInput type="number" min="0"
-                 [disabled]="!maxWidthEnabled.checked"
-                 [ngModel]="dimensions.maxWidth"
-                 (ngModelChange)="updateDimensionProperty('maxWidth', $event)">
-        </mat-form-field>
+        <ng-container *ngIf="dimensions.maxWidth !== undefined">
+          <mat-checkbox #maxWidthEnabled [checked]="dimensions.maxWidth !== null"
+                        (change)="toggleProperty('maxWidth', $event.checked)">
+            {{'propertiesPanel.maxWidthEnabled' | translate }}
+          </mat-checkbox>
+          <mat-form-field>
+            <mat-label>
+              {{'propertiesPanel.maxWidth' | translate }}
+            </mat-label>
+            <input matInput type="number" min="0"
+                   [disabled]="!maxWidthEnabled.checked"
+                   [ngModel]="dimensions.maxWidth"
+                   (ngModelChange)="updateDimensionProperty('maxWidth', $event)">
+          </mat-form-field>
+        </ng-container>
 
-        <mat-checkbox #minHeightEnabled [checked]="dimensions.minHeight !== null"
-                      (change)="toggleProperty('minHeight', $event.checked)">
-          {{'propertiesPanel.minHeightEnabled' | translate }}
-        </mat-checkbox>
-        <mat-form-field>
-          <mat-label>
-            {{'propertiesPanel.minHeight' | translate }}
-          </mat-label>
-          <input matInput type="number" min="0"
-                 [disabled]="!minHeightEnabled.checked"
-                 [ngModel]="dimensions.minHeight"
-                 (ngModelChange)="updateDimensionProperty('minHeight', $event)">
-        </mat-form-field>
+        <ng-container *ngIf="dimensions.minHeight !== undefined">
+          <mat-checkbox #minHeightEnabled [checked]="dimensions.minHeight !== null"
+                        (change)="toggleProperty('minHeight', $event.checked)">
+            {{'propertiesPanel.minHeightEnabled' | translate }}
+          </mat-checkbox>
+          <mat-form-field>
+            <mat-label>
+              {{'propertiesPanel.minHeight' | translate }}
+            </mat-label>
+            <input matInput type="number" min="0"
+                   [disabled]="!minHeightEnabled.checked"
+                   [ngModel]="dimensions.minHeight"
+                   (ngModelChange)="updateDimensionProperty('minHeight', $event)">
+          </mat-form-field>
+        </ng-container>
 
-        <mat-checkbox #maxHeightEnabled [checked]="dimensions.maxHeight !== null"
-                      (change)="toggleProperty('maxHeight', $event.checked)">
-          {{'propertiesPanel.maxHeightEnabled' | translate }}
-        </mat-checkbox>
-        <mat-form-field>
-          <mat-label>
-            {{'propertiesPanel.maxHeight' | translate }}
-          </mat-label>
-          <input matInput type="number" min="0"
-                 [disabled]="!maxHeightEnabled.checked"
-                 [ngModel]="dimensions.maxHeight"
-                 (ngModelChange)="updateDimensionProperty('maxHeight', $event)">
-        </mat-form-field>
+        <ng-container *ngIf="dimensions.maxHeight !== undefined">
+          <mat-checkbox #maxHeightEnabled [checked]="dimensions.maxHeight !== null"
+                        (change)="toggleProperty('maxHeight', $event.checked)">
+            {{'propertiesPanel.maxHeightEnabled' | translate }}
+          </mat-checkbox>
+          <mat-form-field>
+            <mat-label>
+              {{'propertiesPanel.maxHeight' | translate }}
+            </mat-label>
+            <input matInput type="number" min="0"
+                   [disabled]="!maxHeightEnabled.checked"
+                   [ngModel]="dimensions.maxHeight"
+                   (ngModelChange)="updateDimensionProperty('maxHeight', $event)">
+          </mat-form-field>
+        </ng-container>
       </ng-container>
 
       <ng-template #elseBlock>

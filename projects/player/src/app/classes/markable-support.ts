98f61671c12541d7573df5e1f8dc1287fe77890f
jojohoch <joachim.hoch@iqb.hu-berlin.de>
2024-10-11 16:50:40 +0200

    Refactor MarkableSupport
    - Replace foreach loops with map methods

--- projects/player/src/app/classes/markable-support.ts
+++ projects/player/src/app/classes/markable-support.ts
@@ -8,9 +8,8 @@ import {
 export class MarkableSupport {
   private renderer: Renderer2;
   private applicationRef: ApplicationRef;
-  markables: Markable[] = [];
 
-  static wordsWithWhitespace: RegExp = /(\s*\S+\s*)|(s+\S*\s*)|(s*\S*\s+)/g;
+  static wordsWithWhitespace: RegExp = /(\W*\w+\W*)|(W+\w*\W*)|(W*\w*\W+)/gu;
   static prefix: RegExp = /\W+(?=\w+)/u;
   static word: RegExp = /\w+/u;
   static suffix: RegExp = /\W+$/u;
@@ -24,96 +23,96 @@ export class MarkableSupport {
   }
 
   createMarkables(savedMarks: string[], elementComponent: TextComponent): void {
-    const nodes = MarkableSupport.findNodes(elementComponent.textContainerRef.nativeElement.childNodes);
+    const nodes = MarkableSupport.getNodes(elementComponent.textContainerRef.nativeElement.childNodes);
     const markablesContainers = MarkableSupport.getMarkablesContainers(nodes, savedMarks);
-    this.markables = markablesContainers
+    const markables = markablesContainers
       .flatMap((markablesContainer: MarkablesContainer) => markablesContainer.markables);
-    this.createComponents(markablesContainers, elementComponent);
+    this.createComponents(markablesContainers, elementComponent, markables);
   }
 
-  private createComponents(markablesContainers: MarkablesContainer[], elementComponent: TextComponent): void {
-    markablesContainers.forEach((markablesContainer: MarkablesContainer) => {
-      const node = markablesContainer.node;
-      const markableContainerElement = this.renderer.createElement('markable-container');
-      node.parentNode?.replaceChild(markableContainerElement, node);
-      const environmentInjector = this.applicationRef.injector;
-      const componentRef = createComponent(MarkablesContainerComponent, {
-        environmentInjector,
-        hostElement: markableContainerElement
-      });
-      componentRef.instance.markables = markablesContainer.markables;
-      componentRef.instance.selectedColor = elementComponent.selectedColor;
-      componentRef.instance.markablesChange.subscribe(() => {
-        elementComponent.elementValueChanged.emit(
-          {
-            id: elementComponent.elementModel.id,
-            value: this.markables
-          }
-        );
-      });
-      this.applicationRef.attachView(componentRef.hostView);
+  private createComponents(markablesContainers: MarkablesContainer[],
+                           elementComponent: TextComponent,
+                           markables: Markable[]): void {
+    markablesContainers.map((markablesContainer: MarkablesContainer) => this
+      .createMarkablesContainer(markablesContainer, elementComponent, markables)
+    );
+  }
+
+  private createMarkablesContainer(markablesContainer: MarkablesContainer,
+                                   elementComponent: TextComponent,
+                                   markables: Markable[]): void {
+    const node = markablesContainer.node;
+    const markableContainerElement = this.renderer.createElement('markable-container');
+    node.parentNode?.replaceChild(markableContainerElement, node);
+    const environmentInjector = this.applicationRef.injector;
+    const componentRef = createComponent(MarkablesContainerComponent, {
+      environmentInjector,
+      hostElement: markableContainerElement
     });
+    componentRef.instance.markables = markablesContainer.markables;
+    componentRef.instance.selectedColor = elementComponent.selectedColor;
+    componentRef.instance.markablesChange.subscribe(() => {
+      elementComponent.elementValueChanged.emit(
+        {
+          id: elementComponent.elementModel.id,
+          value: markables
+        }
+      );
+    });
+    this.applicationRef.attachView(componentRef.hostView);
   }
 
   private static getMarkablesContainers(nodes: Node[], savedMarks: string[]): MarkablesContainer[] {
-    const markablesContainers: MarkablesContainer[] = [];
-    let wordsCount = 0;
-    nodes.forEach((node: Node) => {
-      const currentNodes = MarkableSupport.getMarkablesContainer(node, wordsCount, savedMarks);
-      wordsCount += currentNodes.markables.length;
-      markablesContainers.push(currentNodes);
+    let offset = 0;
+    return nodes.map((node: Node) => {
+      const currentNodes = MarkableSupport.getMarkablesContainer(node, offset, savedMarks);
+      offset += currentNodes.markables.length;
+      return currentNodes;
     });
-    return markablesContainers;
   }
 
-  private static getMarkablesContainer(node: Node, wordsCount: number, savedMarks: string[]): MarkablesContainer {
+  private static getMarkablesContainer(node: Node, offset: number, savedMarks: string[]): MarkablesContainer {
     return {
       node: node,
-      markables: MarkableSupport.getMarkables(node.textContent || '', wordsCount, savedMarks)
+      markables: MarkableSupport.getMarkables(node.textContent || '', offset, savedMarks)
     };
   }
 
   private static getMarkables(text: string, startIndex: number, savedMarks: string[]): Markable[] {
-    const markables: Markable[] = [];
     const wordsWithWhitespace = text?.match(MarkableSupport.wordsWithWhitespace);
-    wordsWithWhitespace?.forEach((wordWithWhitespace: string, index: number) => {
+    return wordsWithWhitespace?.map((wordWithWhitespace: string, index: number) => {
       const prefix = wordWithWhitespace.match(MarkableSupport.prefix);
       const word = wordWithWhitespace.match(MarkableSupport.word);
       const suffix = wordWithWhitespace.match(MarkableSupport.suffix);
       const id = startIndex + index;
       const color = MarkableSupport.getColorValueById(id, savedMarks);
-      markables.push(
-        {
-          id: id,
-          prefix: prefix ? prefix[0] : '',
-          word: word ? word[0] : '',
-          suffix: suffix ? suffix[0] : '',
-          isActive: !!(word && word[0].length),
-          color: color
-        }
-      );
-    });
-    return markables;
+      return {
+        id: id,
+        prefix: prefix ? prefix[0] : '',
+        word: word ? word[0] : '',
+        suffix: suffix ? suffix[0] : '',
+        isActive: !!(word && word[0].length),
+        color: color
+      };
+    }) || [];
   }
 
-  private static findNodes(childList: Node[] | NodeListOf<ChildNode>): Node[] {
-    const nodes: Node[] = [];
-    childList.forEach((node: Node) => {
+  private static getNodes(childList: Node[] | NodeListOf<ChildNode>): Node[] {
+    return Array.from(childList).reduce((nodes: Node[], node: Node) => {
       if (node.nodeType === Node.TEXT_NODE && !nodes.includes(node) && node.textContent) {
         nodes.push(node);
       }
       if (node.nodeType === Node.ELEMENT_NODE) {
         if (node.childNodes.length) {
-          nodes.push(...MarkableSupport.findNodes(node.childNodes));
+          nodes.push(...MarkableSupport.getNodes(node.childNodes));
         }
       }
-    });
-    return nodes;
+      return nodes;
+    }, []);
   }
 
   private static getColorValueById(id: number, savedMarks: string[]): string | null {
-    const savedMarkById = savedMarks.map(savedMark => savedMark.split('-'))
-      .find(mark => mark[0] === id.toString());
-    return savedMarkById ? savedMarkById[2] : null;
+    return savedMarks.map(savedMark => savedMark.split('-'))
+      .find(mark => mark[0] === id.toString())?.[2] || null;
   }
 }

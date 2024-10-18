be9dff990e767c1f901363de8cdfeacabb69e611
jojohoch <joachim.hoch@iqb.hu-berlin.de>
2024-10-18 15:03:08 +0200

    Extend spinner component with timeout event

--- projects/common/components/spinner/spinner.component.ts
+++ projects/common/components/spinner/spinner.component.ts
@@ -1,7 +1,7 @@
 import {
-  ChangeDetectorRef, Component, Input, OnInit
+  ChangeDetectorRef, Component, EventEmitter, Input, OnDestroy, OnInit, Output
 } from '@angular/core';
-import { Subject } from 'rxjs';
+import { BehaviorSubject, Subject } from 'rxjs';
 import { takeUntil } from 'rxjs/operators';
 
 @Component({
@@ -11,25 +11,39 @@ import { takeUntil } from 'rxjs/operators';
   `,
   styles: ['mat-spinner {margin: auto; position: relative; z-index: 100; top: -50%;}']
 })
-export class SpinnerComponent implements OnInit {
-  @Input() isLoaded!: Subject<boolean>;
+export class SpinnerComponent implements OnInit, OnDestroy {
+  @Input() isLoaded!: BehaviorSubject<boolean>;
+  @Input() timeOutDuration: number = 20000;
+  @Output() timeOut: EventEmitter<number> = new EventEmitter<number>();
+
   isLoading: boolean = true;
   private ngUnsubscribe = new Subject<void>();
+  private timeOutId: number = 0;
 
   constructor(private changeDetectionRef: ChangeDetectorRef) {}
 
   ngOnInit(): void {
+    this.timeOutId = window.setTimeout(() => this.sendTimeOut(), this.timeOutDuration);
     this.isLoaded
       .pipe(takeUntil(this.ngUnsubscribe))
       .subscribe(isLoaded => {
         if (isLoaded) {
+          if (this.timeOutId) clearTimeout(this.timeOutId);
           this.isLoading = false;
           this.changeDetectionRef.detectChanges();
         }
       });
   }
 
+  sendTimeOut(): void {
+    if (!this.isLoaded.value) {
+      this.timeOut.emit(this.timeOutDuration);
+    }
+    clearTimeout(this.timeOutId);
+  }
+
   ngOnDestroy(): void {
+    if (this.timeOutId) clearTimeout(this.timeOutId);
     this.ngUnsubscribe.next();
     this.ngUnsubscribe.complete();
   }

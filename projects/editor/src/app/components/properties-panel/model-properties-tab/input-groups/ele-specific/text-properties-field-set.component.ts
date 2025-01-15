521738d1042064bef0148252d0e82a3fb211c6cd
rhenck <richard.henck@iqb.hu-berlin.de>
2025-01-15 21:48:47 +0100

    [editor] Text: Allow editing by clicking the text on the props panel

--- projects/editor/src/app/components/properties-panel/model-properties-tab/input-groups/ele-specific/text-properties-field-set.component.ts
+++ projects/editor/src/app/components/properties-panel/model-properties-tab/input-groups/ele-specific/text-properties-field-set.component.ts
@@ -30,7 +30,7 @@ import { DialogService } from '../../../../../services/dialog.service';
   template: `
     <div class="fx-column-start-stretch">
       Text
-      <div class="text-text"
+      <div class="text-text" (click)="showTextEditDialog()" [style.cursor]="'pointer'"
            [innerHTML]="combinedProperties.text | safeResourceHTML">
       </div>
       <button mat-fab color="primary" [style.align-self]="'center'" [style.margin-bottom.px]="20"

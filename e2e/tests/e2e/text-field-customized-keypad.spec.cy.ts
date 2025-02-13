a2158ed94059542ac9c03899745daddf60ac38c5
Yan Ping Chen Lin <yanpingchenlin@gmail.com>
2025-02-13 17:50:23 +0100

    Merge remote-tracking branch 'origin/e2e/options' into e2e/options
    # Conflicts:
    #	e2e/tests/e2e/checkbox.spec.cy.ts
    #	e2e/tests/e2e/likert-util.ts
    #	e2e/tests/e2e/likert.spec.cy.ts
    #	e2e/tests/e2e/radio-button-group.spec.cy.ts
    #	e2e/tests/e2e/slider.spec.cy.ts
    #	e2e/tests/e2e/text-field-basic.spec.cy.ts
    #	e2e/tests/e2e/text-field-customized-keypad.spec.cy.ts
    #	e2e/tests/e2e/text-field-util.ts
    #	e2e/tests/e2e/text-range.spec.cy.ts
    #	e2e/tests/e2e/text-selection.spec.cy.ts
    #	e2e/tests/e2e/text-util.ts
    #	e2e/tests/e2e/text-words.spec.cy.ts

--- e2e/tests/e2e/text-field-customized-keypad.spec.cy.ts
+++ e2e/tests/e2e/text-field-customized-keypad.spec.cy.ts
@@ -9,11 +9,20 @@ describe('Text field element', { testIsolation: false }, () => {
 
     it('creates a text field with customized keypad', () => {
       addElement('Eingabefeld');
-      addProperties('Eigene Zeichen', { required: true });
+      addProperties('Eingabefeld mit eigene Zeichen', { required: true });
       addSettings(3, 20, '', { clearable: true, hasKeyboardIcon: true });
-      addMuster('ö12');
-      addHelp('Eigene Zeichen', 'rechts', undefined, 'öäü12');
+      addMuster('123');
+      addHelp('Eigene Zeichen', 'rechts', undefined, '12345');
     });
+
+    it('creates a text field with the same customized keypad disabling other characters', () => {
+      addElement('Eingabefeld');
+      addProperties('Eingabefeld mit Bearbeitung anderer Zeichen verhindern', { required: true });
+      addSettings(3, 20, '', { clearable: true, hasKeyboardIcon: true });
+      addMuster('123');
+      addHelp('Eigene Zeichen', 'rechts', undefined, '12345', {disableOtherCharacters:true});
+    });
+
     after('save an unit definition', () => {
       cy.saveUnit('e2e/downloads/text-field-customized-keypad.json');
     });
@@ -25,29 +34,52 @@ describe('Text field element', { testIsolation: false }, () => {
       cy.loadUnit('../downloads/text-field-customized-keypad.json');
     });
 
-    it('clicks at the text field, and appears the keypad with customized characters', () => {
+    it('clicks at the first text field, and appears the keypad with customized characters', () => {
       cy.get('aspect-keypad').should('not.exist');
-      cy.contains('mat-form-field', 'Eigene Zeichen').click();
-      cy.get('aspect-keypad').contains('ö').should('exist');
-      cy.get('aspect-keypad').contains('p').should('not.exist');
+      cy.contains('mat-form-field', 'Eingabefeld mit eigene Zeichen').click();
+      cy.get('aspect-keypad').contains('1').should('exist');
+      cy.get('aspect-keypad').contains('7').should('not.exist');
     });
 
-    it('types the wrong answer', () => {
-      cy.contains('mat-form-field', 'Eigene Zeichen')
+    it('types the wrong answer in the first text field', () => {
+      cy.contains('mat-form-field', 'Eingabefeld mit eigene Zeichen')
         .find('input')
         .clear()
-        .type('4444{enter}');
-      cy.contains('mat-form-field', 'Eigene Zeichen')
+        .type('6666{enter}');
+      cy.contains('mat-form-field', 'Eingabefeld mit eigene Zeichen')
         .find('mat-error').should('exist');
     });
 
-    it('types the correct answer', () => {
-      cy.contains('mat-form-field', 'Eigene Zeichen')
+    it('types the correct answer in the first text field', () => {
+      cy.contains('mat-form-field', 'Eingabefeld mit eigene Zeichen')
         .find('input')
         .clear()
-        .type('ö12{enter}');
-      cy.contains('mat-form-field', 'Eigene Zeichen')
+        .type('123{enter}');
+      cy.contains('mat-form-field', 'Eingabefeld mit eigene Zeichen')
         .find('mat-error').should('not.exist');
+    });
+
+    it('types the wrong answer with allowed characters in the second text field', () => {
+      cy.contains('mat-form-field', 'Eingabefeld mit Bearbeitung anderer Zeichen verhindern')
+        .find('input').click();
+      cy.contains('mat-form-field', 'Eingabefeld mit eigene Zeichen')
+        .find('input').click();
+
+      cy.contains('mat-form-field', 'Eingabefeld mit Bearbeitung anderer Zeichen verhindern')
+        .find('input')
+        .clear()
+        .type('5555{enter}');
+      cy.contains('mat-form-field', 'Eingabefeld mit Bearbeitung anderer Zeichen verhindern')
+        .find('mat-error').should('exist');
+    });
+
+    it('should not type non allowed characters in the second text field', () => {
+      cy.contains('mat-form-field', 'Eingabefeld mit Bearbeitung anderer Zeichen verhindern')
+        .find('input')
+        .clear()
+        .type('7777{enter}');
+      cy.contains('mat-form-field', 'Eingabefeld mit Bearbeitung anderer Zeichen verhindern')
+        .find('mat-error').contains('7777').should('not.exist');
     });
   });
 });

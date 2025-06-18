c71649a67d34238da04870450b2ec76b31de3104
jojohoch <joachim.hoch@iqb.hu-berlin.de>
2025-06-18 15:48:51 +0200

    [player] Add print labels for child elements

--- projects/player/modules/print/components/print-element/print-element.component.ts
+++ projects/player/modules/print/components/print-element/print-element.component.ts
@@ -1,7 +1,10 @@
-import { Component, Input, OnInit, ViewChild, ViewContainerRef } from '@angular/core';
+import {
+  Component, Input, OnInit, Renderer2, ViewChild, ViewContainerRef
+} from '@angular/core';
 import { UIElement } from 'common/models/elements/element';
 import { ElementComponent } from 'common/directives/element-component.directive';
 import { PrintMode } from 'player/modules/verona/models/verona';
+import { CompoundElementComponent } from 'common/directives/compound-element.directive';
 
 @Component({
   selector: 'aspect-print-element',
@@ -17,24 +20,28 @@ export class PrintElementComponent implements OnInit {
              { read: ViewContainerRef, static: true }) private elementComponentContainer!: ViewContainerRef;
 
   printElementComponent!: ElementComponent;
-
-  children: UIElement[] = [];
-  tooltipText: string = '';
+  constructor(private renderer: Renderer2) {
+  }
 
   ngOnInit(): void {
     if (this.elementModel) {
       this.printElementComponent =
         this.elementComponentContainer.createComponent(this.elementModel.getElementComponent()).instance;
       this.printElementComponent.elementModel = this.elementModel;
-      this.children = this.elementModel.getChildElements();
-      this.tooltipText = this.getTooltipText();
-    }
-  }
 
-  getTooltipText(): string {
-    return [
-      this.elementModel.alias,
-      ...this.children.map(child => child.alias)
-    ].join('\n');
+      if (this.printElementComponent instanceof CompoundElementComponent) {
+        (this.printElementComponent as CompoundElementComponent).childrenAdded
+          .subscribe((children: ElementComponent[]) => {
+            children.forEach(child => {
+              child.domElement.setAttribute('data-element-id', child.elementModel.id);
+              child.domElement.setAttribute('data-element-alias', child.elementModel.alias);
+              const label: HTMLElement = this.renderer.createElement('span');
+              label.innerHTML = child.elementModel.alias;
+              this.renderer.addClass(label, 'element-label');
+              this.renderer.appendChild(child.domElement, label);
+            });
+          });
+      }
+    }
   }
 }

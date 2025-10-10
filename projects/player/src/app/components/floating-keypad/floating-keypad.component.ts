30ed4e44ea32374054352ad613d0e7e6a99274e1
jojohoch <joachim.hoch@iqb.hu-berlin.de>
2025-10-10 15:19:03 +0200

    [player] Fix dynamic assignment of cdkConnectedOverlayOrigins
    - Keypad
    - Marking of Text

--- projects/player/src/app/components/floating-keypad/floating-keypad.component.ts
+++ projects/player/src/app/components/floating-keypad/floating-keypad.component.ts
@@ -6,10 +6,10 @@ import { UIElementType } from 'common/interfaces';
 import { KeypadService } from '../../services/keypad.service';
 
 @Component({
-    selector: 'aspect-floating-keypad',
-    templateUrl: './floating-keypad.component.html',
-    styleUrls: ['./floating-keypad.component.scss'],
-    standalone: false
+  selector: 'aspect-floating-keypad',
+  templateUrl: './floating-keypad.component.html',
+  styleUrls: ['./floating-keypad.component.scss'],
+  standalone: false
 })
 export class FloatingKeypadComponent implements OnChanges {
   @Input() isKeypadOpen!: boolean;

acd6d3e71601ca9b4079c26b2b4d7d54ce7f224b
rhenck <richard.henck@iqb.hu-berlin.de>
2025-10-26 21:15:00 +0100

    Fix not failing unit tests
    There were several runtime errors in the logs that were not considered
    problematic. Now fails when encountering those.

--- projects/editor/src/test.ts
+++ projects/editor/src/test.ts
@@ -12,6 +12,8 @@ getTestBed().initTestEnvironment(
   BrowserDynamicTestingModule,
   platformBrowserDynamicTesting(),
   {
+    errorOnUnknownElements: true,
+    errorOnUnknownProperties: true,
     teardown: { destroyAfterEach: false }
   }
 );

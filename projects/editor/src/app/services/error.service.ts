7c60dd9cf71e7ce078f404887190533940b79278
jojohoch <joachim.hoch@iqb.hu-berlin.de>
2025-11-14 10:07:42 +0100

    [editor] Handle AspectErrors used in player to communicate with host

--- projects/editor/src/app/services/error.service.ts
+++ projects/editor/src/app/services/error.service.ts
@@ -1,16 +1,23 @@
-import { ErrorHandler, Injectable, NgZone } from '@angular/core';
+import { ErrorHandler, Injectable } from '@angular/core';
 import { IDError } from 'common/errors';
 import { MessageService } from 'editor/src/app/services/message.service';
+import { AspectError } from 'common/classes/aspect-error';
+import { TranslateService } from '@ngx-translate/core';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ErrorService implements ErrorHandler {
-  constructor(private messageService: MessageService) { }
+  constructor(
+    private translateService: TranslateService,
+    private messageService: MessageService) { }
 
   handleError(error: Error): void {
     if (error instanceof IDError) {
       error.highSeverity ? this.messageService.showPrompt(error.message) : this.messageService.showError(error.message);
+    } else if (error instanceof AspectError && error.code !== 'geogebra-not-loading') {
+      this.messageService
+        .showPrompt(this.translateService.instant('error.corruptElement', { errorMsg: error.message }));
     } else {
       this.messageService.showErrorPrompt(error);
       // eslint-disable-next-line no-console

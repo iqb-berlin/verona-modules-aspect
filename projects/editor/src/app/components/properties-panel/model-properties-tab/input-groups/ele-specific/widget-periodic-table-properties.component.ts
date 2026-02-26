8bf0d8fc5db28770082566387fcfb41d5cf3ed9d
jojohoch <joachim.hoch@iqb.hu-berlin.de>
2026-02-26 09:44:01 +0100

    [editor][widgets] Add fieldsets to properties panel

--- projects/editor/src/app/components/properties-panel/model-properties-tab/input-groups/ele-specific/widget-periodic-table-properties.component.ts
+++ projects/editor/src/app/components/properties-panel/model-properties-tab/input-groups/ele-specific/widget-periodic-table-properties.component.ts
@@ -16,31 +16,35 @@ import { UIElement } from 'common/models/elements/element';
     MatInputModule
   ],
   template: `
-    <div class="fx-column-start-stretch">
-      <mat-checkbox [checked]="$any(combinedProperties).showInfoOrder"
-                    (change)="updateModel.emit({ property: 'showInfoOrder', value: $event.checked })">
-        {{ 'propertiesPanel.showInfoOrder' | translate }}
-      </mat-checkbox>
-      <mat-checkbox [checked]="$any(combinedProperties).showInfoENeg"
-                    (change)="updateModel.emit({ property: 'showInfoENeg', value: $event.checked })">
-        {{ 'propertiesPanel.showInfoENeg' | translate }}
-      </mat-checkbox>
-      <mat-checkbox [checked]="$any(combinedProperties).showInfoAMass"
-                    (change)="updateModel.emit({ property: 'showInfoAMass', value: $event.checked })">
-        {{ 'propertiesPanel.showInfoAMass' | translate }}
-      </mat-checkbox>
-      <mat-checkbox [checked]="$any(combinedProperties).closeOnSelection"
-                    (change)="updateModel.emit({ property: 'closeOnSelection', value: $event.checked })">
-        {{ 'propertiesPanel.closeOnSelection' | translate }}
-      </mat-checkbox>
+    <fieldset>
+      <legend>{{'toolbox.widget-periodic-table' | translate}}</legend>
+      <div class="fx-column-start-stretch">
+        <mat-checkbox [checked]="$any(combinedProperties).showInfoOrder"
+                      (change)="updateModel.emit({ property: 'showInfoOrder', value: $event.checked })">
+          {{ 'propertiesPanel.showInfoOrder' | translate }}
+        </mat-checkbox>
+        <mat-checkbox [checked]="$any(combinedProperties).showInfoENeg"
+                      (change)="updateModel.emit({ property: 'showInfoENeg', value: $event.checked })">
+          {{ 'propertiesPanel.showInfoENeg' | translate }}
+        </mat-checkbox>
+        <mat-checkbox [checked]="$any(combinedProperties).showInfoAMass"
+                      (change)="updateModel.emit({ property: 'showInfoAMass', value: $event.checked })">
+          {{ 'propertiesPanel.showInfoAMass' | translate }}
+        </mat-checkbox>
+        <mat-checkbox [checked]="$any(combinedProperties).closeOnSelection"
+                      (change)="updateModel.emit({ property: 'closeOnSelection', value: $event.checked })">
+          {{ 'propertiesPanel.closeOnSelection' | translate }}
+        </mat-checkbox>
 
-      <mat-form-field appearance="fill">
-        <mat-label>{{'propertiesPanel.maxNumberOfSelections' | translate }}</mat-label>
-        <input matInput type="number" min="0"
-               [value]="$any(combinedProperties).maxNumberOfSelections"
-               (input)="updateModel.emit({ property: 'maxNumberOfSelections', value: $any($event.target).value || 0 })">
-      </mat-form-field>
-    </div>
+        <mat-form-field appearance="fill">
+          <mat-label>{{'propertiesPanel.maxNumberOfSelections' | translate }}</mat-label>
+          <input matInput type="number" min="0"
+             [value]="$any(combinedProperties).maxNumberOfSelections"
+             (input)="updateModel
+               .emit({ property: 'maxNumberOfSelections', value: $any($event.target).value || 0 })">
+        </mat-form-field>
+      </div>
+    </fieldset>
   `
 })
 export class WidgetPeriodicTablePropertiesComponent {

0d376aeaf818d739358d8fdea432c6fce347feee
jojohoch <joachim.hoch@iqb.hu-berlin.de>
2026-04-28 10:00:20 +0200

    [editor]  Revise the solution to fix a race condition during loading

--- projects/editor/src/app/app.component.ts
+++ projects/editor/src/app/app.component.ts
@@ -1,28 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
 import { registerLocaleData } from '@angular/common';
 import localeDe from '@angular/common/locales/de';
+import { Subscription } from 'rxjs';
 import { VeronaAPIService, StartCommand } from './services/verona-api.service';
 import { UnitService } from './services/unit.service';
 
 @Component({
-    selector: 'aspect-editor',
-    template: `
+  selector: 'aspect-editor',
+  template: `
     <div class="mainView fx-column-start-stretch">
       <aspect-toolbar *ngIf="isStandalone"></aspect-toolbar>
       <aspect-editor-unit-view class="fx-flex"></aspect-editor-unit-view>
     </div>
   `,
-    styles: [`
+  styles: [`
     .mainView {
       height: 100%;
     }
   `],
-    standalone: false
+  standalone: false
 })
 
-export class AppComponent implements OnInit {
+export class AppComponent implements OnInit, OnDestroy {
   isStandalone = window === window.parent;
+  private loadingSubscription: Subscription | undefined;
 
   constructor(private unitService: UnitService,
               private translateService: TranslateService,
@@ -34,6 +36,11 @@ export class AppComponent implements OnInit {
   ngOnInit(): void {
     this.veronaApiService.startCommand
       .subscribe((message: StartCommand): void => {
+        // Unsubscribe from previous loading operation if it exists
+        if (this.loadingSubscription) {
+          this.loadingSubscription.unsubscribe();
+        }
+
         this.unitService.loadUnitDefinition(message.unitDefinition);
         if (message.editorConfig.role === 'developer') {
           this.unitService.expertMode = false;
@@ -42,5 +49,11 @@ export class AppComponent implements OnInit {
       });
     this.veronaApiService.sendReady();
     registerLocaleData(localeDe);
+  }
+
+  ngOnDestroy(): void {
+    if (this.loadingSubscription) {
+      this.loadingSubscription.unsubscribe();
+    }
   }
 }
